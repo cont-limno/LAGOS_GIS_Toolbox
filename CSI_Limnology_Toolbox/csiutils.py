4c652636fec073f907bbeef9bbdce3030eff4504
nicolejeansmith <nicole.j.smith@gmail.com>
2014-05-27 10:06:36 -0400

    Added functions rename_field: simulates a field rename in arcgis one_in_one_out: ensures that for every input feature there is an output record because ArcGIS tools sometimes create no row if there is "no" value redefine_nulls: updates null values in output tables to our custom value, like 'NA' (text) or 0, as needed
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/csiutils.py
+++ CSI_Limnology_Toolbox/csiutils.py
@@ -43,3 +43,71 @@ def directory_size(directory):
                 total_size += os.path.getsize(f_abspath)
     return float(total_size)
 
+def merge_many(merge_list, out_fc, group_size = 20):
+    """arcpy merge a list without blowing up your system
+        this can be slow, but is usually better than the alternative
+        if there are more than x (usually 20) files to merge, merge them in
+        groups of 20 at a time to speed it up some"""
+    if len(merge_list) > group_size:
+        partitions = 1 + len(merge_list) // (group_size + 1)
+        multi_msg("Merging partition 1 of %s" % partitions)
+        arcpy.Merge_management(merge_list[:group_size], out_fc)
+        for n in range(partitions)[1:]:
+            multi_msg("Merging partition %s of %s" % (n + 1, partitions))
+            arcpy.Append_management(merge_list[group_size*n:group_size*(1+n)], out_fc)
+    else:
+        arcpy.Merge_management(merge_list, out_fc)
+
+def rename_field(inTable, oldFieldName, newFieldName, deleteOld = False):
+    import arcpy
+    old_field = arcpy.ListFields(inTable, oldFieldName)
+
+    arcpy.AddField_management(inTable, newFieldName, old_field[0].type, field_length = old_field[0].length)
+    arcpy.CalculateField_management(inTable, newFieldName,'!%s!' % oldFieldName, "PYTHON")
+    if deleteOld == True: arcpy.DeleteField_management(inTable, oldFieldName)
+
+
+def one_in_one_out(tool_table, calculated_fields, extent_fc, zone_field, output_table):
+    """ Occasionally, ArcGIS tools we use do not produce an output record for
+    every input feature. This function is used in the toolbox whenever we need
+    to correct this problem, and should be called at the end of the script to
+    create the final output.
+    tool_table: the intermediate table with missing features
+    extent_fc: the feature class with the zones
+    zone_field: the field uniquely identifying each feature that was used in
+    the creation of tool_table. Because this function is called within our
+    scripts, the zone_field should always be the same in tool_table and
+    extent_fc
+    output_table: the final output table
+    tool_fields: the calculated fields you want to retain, besides zone_field
+    """
+
+    # This function is mostly a hack on an outer right join. Want to join to
+    # extent_fc but select ONLY the ID field, and keep all records in extent_fc
+    # If you find a better way, update this.
+    arcpy.CopyRows_management(extent_fc, output_table)
+    arcpy.JoinField_management(output_table, zone_field, tool_table, zone_field)
+    calculated_fields.append(zone_field)
+    field_names = [f.name for f in arcpy.ListFields(output_table)]
+    for f in field_names:
+        if f not in calculated_fields:
+            try:
+                arcpy.DeleteField_management(output_table, f)
+            except:
+                continue
+
+def redefine_nulls(in_table, in_fields, out_values):
+    """Sometimes, when a zone has nothing in it, it gets an output value of
+    Null/NoData/None that we need to change to either a custom NA flag, or 0.
+    in_table: the table that needs updating. It will be changed in place.
+    in_fields: list of fields with null values that need updating
+    out_values: a list the same length as in_fields, with the value to replace
+    Null/NoData/None with.
+    """
+    arcpy.MakeTableView_management(in_table, 'table')
+    for f, v in zip(in_fields, out_values):
+        null_expr = '''"{0}" is null'''.format(f)
+        arcpy.SelectLayerByAttribute_management('table', 'NEW_SELECTION', null_expr)
+        calc_expr = v
+        arcpy.CalculateField_management('table', f, calc_expr, 'PYTHON')
+

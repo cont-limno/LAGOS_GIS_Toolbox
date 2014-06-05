a1c7a2054dfbf7ba8d8be469df398f7c0bd9acf9
nicolejeansmith <nicole.j.smith@gmail.com>
2014-06-05 12:59:51 -0400

    Minor changes to polygons_in_zones
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/polygons_in_zones.py
+++ CSI_Limnology_Toolbox/polygons_in_zones.py
@@ -12,7 +12,8 @@ import os
 import arcpy
 import csiutils as cu
 
-def polygons_in_zones(zone_fc, zone_field, polygons_of_interest, interest_selection_expr, output_table):
+def polygons_in_zones(zone_fc, zone_field, polygons_of_interest, output_table, interest_selection_expr):
+    env.workspace = 'in_memory'
     if interest_selection_expr:
         arcpy.MakeFeatureLayer_management(polygons_of_interest, "selected_polys", interest_selection_expr)
     else:
@@ -24,8 +25,8 @@ def polygons_in_zones(zone_fc, zone_field, polygons_of_interest, interest_select
     tab_table = 'tabulate_intersection_table'
     arcpy.TabulateIntersection_analysis(zone_fc, zone_field, "selected_polys",
                                         tab_table)
-    cu.rename_field(tab_table, 'AREA', 'WL_AREA_ha', True)
-    cu.rename_field(tab_table, 'PERCENTAGE', 'WL_AREA_pct', True)
+    cu.rename_field(tab_table, 'AREA', 'Poly_AREA_ha', True)
+    cu.rename_field(tab_table, 'PERCENTAGE', 'Poly_AREA_pct', True)
     spjoin_fc = 'spatial_join_output'
     field_mapping ="""{0} "{0}" true true false 20 Text 0 0 ,First,#,
                     {1},
@@ -35,7 +36,7 @@ def polygons_in_zones(zone_fc, zone_field, polygons_of_interest, interest_select
                                  "JOIN_ONE_TO_ONE", "KEEP_ALL",
                                  field_mapping, "INTERSECT")
     arcpy.AddField_management(spjoin_fc, "WL_Count", 'Short')
-    arcpy.CalculateField_management(spjoin_fc, "WL_Count", '!Join_Count!', "PYTHON")
+    arcpy.CalculateField_management(spjoin_fc, "Poly_Count", '!Join_Count!', "PYTHON")
 
     arcpy.JoinField_management(tab_table, zone_field, spjoin_fc, zone_field, "WL_Count")
     final_fields = ['WL_AREA_ha', 'WL_AREA_pct', 'WL_Count']
@@ -50,7 +51,6 @@ def main():
     polygons_of_interest = arcpy.GetParameterAsText(2)
     interest_selection_expr = arcpy.GetParameterAsText(3) # default should be set to """"ATTRIBUTE" LIKE 'P%'AND "WETLAND_TY" <> 'Freshwater_Pond'"""
     output_table = arcpy.GetParameterAsText(4)
-    arcpy.env.workspace = 'in_memory'
     polygons_in_zones(zone_fc, zone_field, polygons_of_interest, interest_selection_expr, output_table)
 
 def test():
@@ -59,7 +59,6 @@ def test():
     polygons_of_interest = 'C:/GISData/Scratch/Scratch.gdb/wetlands_test'
     interest_selection_expr = ''
     output_table = 'C:/GISData/Scratch/Scratch.gdb/polyzone_test'
-    arcpy.env.workspace = 'C:/GISData/Scratch/fake_memory.gdb'
     polygons_in_zones(zone_fc, zone_field, polygons_of_interest, interest_selection_expr, output_table)
 
 if __name__ == '__main__':

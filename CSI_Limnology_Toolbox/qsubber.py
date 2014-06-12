fae1cf5841fbf386c086c5e4dec1054b73fd1a84
nicolejeansmith <nicole.j.smith@gmail.com>
2014-06-12 09:26:06 -0400

    Added python script to create qsub script but it's not usable yet
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

---
+++ CSI_Limnology_Toolbox/qsubber.py
@@ -0,0 +1,33 @@
+import os
+
+def create_squb(raster_dir, qsub_path, tool = ('pitremove', 'd8flowdir'), walltime = '0:30:00', taudem_dir = ''):
+    tif_list = []
+    for root, dirs, files in os.walk(raster_dir):
+        for f in files:
+            if f.endswith('.tif'):
+                if 'fel' not in f and 'sd8' not in f and 'p.tif' not in f:
+                    tif_list.append(os.path.join(root, name))
+
+    for tif in tif_list:
+        with open(qsub_path, 'a') as f:
+            name = os.path.splitext(os.path.basename(tif))[0]
+            f.write("""#!/bin/bash --login
+            #PBS -j oe
+            #PBS -l walltime={0},nodes=8:ppn=1,mem=8gb,feature=gbe
+            #PBS -N {1}
+
+            file={2}
+            echo ${PBS_JOBID} ${file}
+            time mpiexec -n 8 {3} ${file}
+            qstat -f
+            """.format(walltime, name, tif, tool))
+
+##            os.system('qsub {0}'.format(qsub_path))
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

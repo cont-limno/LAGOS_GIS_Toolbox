47f22b92825d5d28d71c195e559bfd0f517487e2
nicolejeansmith <nicole.j.smith@gmail.com>
2014-06-12 09:25:24 -0400

    Made Export2CSV.py contain a callable function Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/Export2CSV.py
+++ CSI_Limnology_Toolbox/Export2CSV.py
@@ -1,28 +1,20 @@
 # Filename: Export2CSV.py
 import arcpy, os, csv
-fc = arcpy.GetParameterAsText(0)
-outfolder = arcpy.GetParameterAsText(1)
 
-name = os.path.splitext(os.path.basename(fc))[0]
-
-csv = os.path.join(outfolder, name + ".csv")
-                        
-
-def TableToCSV(fc,CSVFile):
-    
-    fields = [f.name for f in arcpy.ListFields(fc) if f.type <> 'Geometry' and f.name <> 'Shape_Area' and f.name <> 'Shape_Length' and f.name <> 'TARGET_FID']
-    with open(CSVFile, 'w') as f:
+def TableToCSV(in_table, out_folder):
+    name = os.path.splitext(os.path.basename(in_table))[0]
+    out_csv = os.path.join(out_folder, name + '.csv')
+    fields = [f.name for f in arcpy.ListFields(in_table) if f.type <> 'Geometry' and f.name <> 'Shape_Area' and f.name <> 'Shape_Length' and f.name <> 'TARGET_FID']
+    with open(out_csv, 'w') as f:
         f.write(','.join(fields)+'\n') #csv headers
-        with arcpy.da.SearchCursor(fc, fields) as cursor:
+        with arcpy.da.SearchCursor(in_table, fields) as cursor:
             for row in cursor:
                 f.write(','.join([str(r) for r in row])+'\n')
-    
+
+def main():
+    in_table = arcpy.GetParameterAsText(0)
+    out_folder = arcpy.GetParameterAsText(1)
+    TableToCSV(in_table, out_folder)
 
 if __name__ == '__main__':
-
- 
-    
-
-
-    TableToCSV(fc, csv)
-    
+    main()

1e6f7873d41f7f17f32633d88a1e4009e59b6039
nicolejeansmith <nicole.j.smith@gmail.com>
2014-06-30 12:50:23 -0400

    Null values will now be 'NA' in CSV exports instead of 'None'
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/Export2CSV.py
+++ CSI_Limnology_Toolbox/Export2CSV.py
@@ -1,15 +1,20 @@
 # Filename: Export2CSV.py
-import arcpy, os, csv
+import csv, os
+import arcpy
 
-def TableToCSV(in_table, out_folder):
-    name = os.path.splitext(os.path.basename(in_table))[0]
+def TableToCSV(in_table, out_folder, new_table_name = ''):
+    if new_table_name:
+        name = new_table_name
+    else:
+        name = os.path.splitext(os.path.basename(in_table))[0]
     out_csv = os.path.join(out_folder, name + '.csv')
     fields = [f.name for f in arcpy.ListFields(in_table) if f.type <> 'Geometry' and f.name <> 'Shape_Area' and f.name <> 'Shape_Length' and f.name <> 'TARGET_FID']
     with open(out_csv, 'w') as f:
         f.write(','.join(fields)+'\n') #csv headers
         with arcpy.da.SearchCursor(in_table, fields) as cursor:
             for row in cursor:
-                f.write(','.join([str(r) for r in row])+'\n')
+                values = ['NA' if r is None else str(r) for r in row]
+                f.write(','.join(values)+'\n')
 
 def main():
     in_table = arcpy.GetParameterAsText(0)

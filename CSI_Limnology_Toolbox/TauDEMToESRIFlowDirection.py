4ab9a921bcdd26f319183f7d45c3370bc2efdb79
nicolejeansmith <nicole.j.smith@gmail.com>
2014-07-02 17:22:45 -0400

    TauDEMTOESRIFlowDirection now takes multiple inputs
    You can run as many as you want at one time and the outputs will all be named automatically and written in the same folder
    I thought about simply moving this conversion into the upcoming combined tool "Create Watersheds" but I don't want that tool creating any additional outputs and users will want saved copies of the ESRI flow direction, so separate tool it is
    
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/TauDEMToESRIFlowDirection.py
+++ CSI_Limnology_Toolbox/TauDEMToESRIFlowDirection.py
@@ -3,15 +3,23 @@
 
 import os
 import arcpy
+from arcpy.sa import *
+import csiutils as cu
 arcpy.CheckOutExtension("Spatial")
 
-inraster = arcpy.GetParameterAsText(0)
-outfolder = arcpy.GetParameterAsText(1)
-
-flowras = Raster(inraster)
-
-flowras_reclass = Reclassify(flowras, "Value",
+def taudem_to_esri_flowdir(raster_list, out_dir):
+    for r in raster_list:
+        flowras = Raster(r)
+        flowras_reclass = Reclassify(flowras, "Value",
             RemapValue([[1,1],[2,128],[3,64],[4,32],[5,16],[6,8],[7,4],[8,2]]))
+        out_name = os.path.join(out_dir, os.path.basename(r).replace('p', 'esri_flowdir'))
+        flowras_reclass.save(out_name)
 
-outname = os.path.join(outfolder, "D8FDR" + os.path.basename(inraster)[3:11] + ".tif")
-flowras_reclass.save(outname)
+def main():
+    raster_list = arcpy.GetParameterAsText(0).split(';')
+    print(raster_list)
+    out_dir = arcpy.GetParameterAsText(1)
+    taudem_to_esri_flowdir(raster_list, out_dir)
+
+if __name__ == '__main__':
+    main()

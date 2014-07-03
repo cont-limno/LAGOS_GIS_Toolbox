0dabd435979e9e9cd4b90fcbfebbd891ab41253c
nicolejeansmith <nicole.j.smith@gmail.com>
2014-07-02 20:34:59 -0400

    Updated clip_to_hu8
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/clip_to_hu8.py
+++ CSI_Limnology_Toolbox/clip_to_hu8.py
@@ -1,17 +1,23 @@
 import os, re
 import arcpy
 from arcpy import env
+from arcpy.sa import *
 import csiutils as cu
 
+
 def clip_to_hu8(raster, nhd_gdb, out_dir,
-        projection = arcpy.SpatialReference(102039)):
+                    projection = arcpy.SpatialReference(102039)):
     """Outputs a series of rasters, each one clipped to a different HU8. """
-    env.workspace = nhd_gdb
+    env.workspace = 'in_memory'
     env.outputCoordinateSystem = projection
     env.compression = "NONE" # only final tifs are generated
+    env.snapRaster = raster
+    env.cellSize = '10'
+    env.pyramids = "PYRAMIDS -1 SKIP_FIRST"
+    arcpy.CheckOutExtension("Spatial")
 
     # HUC8 polygons each saved as separate fc inheriting albers from environ
-    huc8_fc = "WBD_HU8"
+    huc8_fc = os.path.join(nhd_gdb, "WBD_HU8")
     arcpy.MakeFeatureLayer_management(huc8_fc, "huc8_layer")
     huc4_code = re.search('\d{4}', os.path.basename(nhd_gdb)).group()
 
@@ -19,47 +25,40 @@ def clip_to_hu8(raster, nhd_gdb, out_dir,
     if not os.path.exists(clips_dir):
         os.mkdir(clips_dir)
 
+##    # add walls
+##    arcpy.PolygonToLine_management(huc8_fc, 'wall_lines')
+##    arcpy.AddField_management('wall_lines', "height", "DOUBLE")
+##    arcpy.CalculateField_management('wall_lines', "height", '500', "PYTHON")
+##    arcpy.FeatureToRaster_conversion('wall_lines', "height", 'wall_raster')
+##    wallsObject = Raster('wall_raster')
+##    elevObject = Raster(raster)
+##    walled_ned = Con(IsNull(wallsObject), elevObject,
+##                    (wallsObject + elevObject))
+
+    # for each HU8 feature in the fc, make a clip
     with arcpy.da.SearchCursor(huc8_fc, ["HUC_8"]) as cursor:
         for row in cursor:
             if row[0].startswith(huc4_code):
                 whereClause = """"{0}" = '{1}'""".format("HUC_8", row[0])
                 arcpy.SelectLayerByAttribute_management("huc8_layer", 'NEW_SELECTION', whereClause)
-                arcpy.CopyFeatures_management("huc8_layer", "in_memory/fc")
+                arcpy.CopyFeatures_management("huc8_layer", "this_hu8")
+
+                # clip the raster
                 out_raster = os.path.join(clips_dir, 'NED{0}.tif'.format(row[0]))
-
-                # instead of the unnecessary 5km buffer and walling process
-                # just going to clip directly to HU8 boundary and set everything
-                # outside HIGHER than any possible elevation value
-                # the point is that flow isn't allowed to cross HU8s, right?
-                # since flowdir calcs are per HU8 anyway
-                # TEST new flowdir outputs against old before keeping this
                 cu.multi_msg('Creating output {0}'.format(out_raster))
+
+                # use a small buffer here because otherwise the walls get
+                # cut off in slivers
+                arcpy.Buffer_analysis('this_hu8', 'this_hu8_buffer', 5000)
                 arcpy.Clip_management(raster, '', out_raster,
-                                    'in_memory/fc', '100000',
+                                    'this_hu8_buffer', '#',
                                     'ClippingGeometry')
-                arcpy.Delete_management('in_memory/fc')
+                arcpy.Delete_management('this_hu8')
+                arcpy.Delete_management('this_hu8_buffer')
 
-##    #retrieve only the single huc8 fcs and not the one with all of them
-##    fcs = arcpy.ListFeatureClasses("HUC%s*" % huc4_code, "Polygon", out_feature_dataset)
-##    fcs_buffered = [os.path.join(out_feature_dataset, fc + "_buffer") for fc in fcs]
-##    out_clips = [os.path.join(outfolder, "huc8clips" + huc4_code,
-##    "NED" + fc[3:] + ".tif") for fc in fcs]
-##
-##    # Buffer HUC8 feature classes by 5000m
-##    for fc, fc_buffered in zip(fcs, fcs_buffered):
-##        arcpy.Buffer_analysis(fc, fc_buffered, "5000 meters")
-##
-##    cu.multi_msg("Created HUC8 buffers.")
-##    arcpy.RefreshCatalog(nhd)
-
-##    # Clips rasters
-##    cu.multi_msg("Starting HUC8 clips...")
-##    for fc_buffered, out_clip in zip(fcs_buffered, out_clips):
-##        arcpy.Clip_management(raster, '', out_clip, fc_buffered, "0", "ClippingGeometry")
-##
-##    arcpy.Compact_management(nhd)
-##
-##    cu.multi_msg("Clipping complete.")
+    arcpy.Delete_management('huc8_layer')
+    arcpy.ResetEnvironments()
+    arcpy.CheckInExtension("Spatial")
 
 
 def main():
@@ -67,11 +66,10 @@ def main():
     nhd_gdb = arcpy.GetParameterAsText(1)
     out_dir = arcpy.GetParameterAsText(2)
     clip_to_hu8(burnt_ned, nhd_gdb, out_dir)
-    arcpy.ResetEnvironments()
 
 
 def test():
-    burnt_ned = 'C:/GISData/Scratch/Burnt_0411.tif'
+    burnt_ned = 'C:/GISData/Scratch/NHD0411/Burnt_0411.tif'
     nhd_gdb = 'C:/GISData/Scratch/NHD0411/NHDH0411.gdb'
     out_dir = 'C:/GISData/Scratch/NHD0411/'
     clip_to_hu8(burnt_ned, nhd_gdb, out_dir)
--- CSI_Limnology_Toolbox/zonal_tabarea.py
+++ CSI_Limnology_Toolbox/zonal_tabarea.py
@@ -119,7 +119,8 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
     # cleanup
     arcpy.Delete_management(temp_zonal_table)
     arcpy.Delete_management(temp_entire_table)
-    arcpy.Delete_management(os.path.dirname(temp_workspace))
+    if arcpy.Exists(temp_workspace):
+        arcpy.Delete_management(os.path.dirname(temp_workspace))
     arcpy.CheckInExtension("Spatial")
 
 def main():

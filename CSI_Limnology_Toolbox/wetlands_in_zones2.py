c9b900bd4119c61bec9e354e652f4bcc57b02374
nicolejeansmith <nicole.j.smith@gmail.com>
2014-08-06 17:59:51 -0400

    Added support for dissolved wetland count in wetlands_in_zones2.py
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/wetlands_in_zones2.py
+++ CSI_Limnology_Toolbox/wetlands_in_zones2.py
@@ -5,7 +5,7 @@ import arcpy
 import polygons_in_zones
 import csiutils as cu
 
-def wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table):
+def wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table, dissolve_wetlands = True):
 
     # make sure we're only using the right types of wetlands, our feature
     # class excludes everything else but this is a guarantee this will
@@ -32,7 +32,7 @@ def wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table):
                 """"VegType" = 'PSS'""",
                 """"VegType" = 'PEMorPAB'""",
                 ]
-    temp_tables = ['WL',
+    temp_tables = ['WL_Undissolved',
                 'WL_Isolated',
                 'WL_Single',
                 'WL_Connected',
@@ -47,44 +47,54 @@ def wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table):
         for f in new_fields:
             cu.rename_field(temp_table, f, f.replace('Poly', temp_table), True)
 
+    if dissolve_wetlands == True:
+        arcpy.Dissolve_management(wetlands_fc, 'dissolved_wetlands', multi_part = 'SINGLE_PART')
+        polygons_in_zones.polygons_in_zones(zones_fc, zone_field, 'dissolved_wetlands', 'WL_Dissolved', '')
+        for f in new_fields:
+            cu.rename_field('WL_Dissolved', f, f.replace('Poly', 'WL_Dissolved'), True)
+        temp_tables.append('WL_Dissolved')
+
     # join em up and copy to final
-    temp_tables.remove('WL')
+    temp_tables.remove('WL_Undissolved')
     for t in temp_tables:
         try:
-            arcpy.JoinField_management('WL', zone_field, t, zone_field)
+            arcpy.JoinField_management('WL_Undissolved', zone_field, t, zone_field)
         # sometimes there's no table if it was an empty selection
         except:
             empty_fields = [f.replace('Poly', t) for f in new_fields]
             for ef in empty_fields:
-                arcpy.AddField_management('WL', ef, 'Double')
-                arcpy.CalculateField_management('WL', ef, '0', 'PYTHON')
+                arcpy.AddField_management('WL_Undissolved', ef, 'Double')
+                arcpy.CalculateField_management('WL_Undissolved', ef, '0', 'PYTHON')
             continue
     # remove all the extra zoneID fields, which have underscore in name
-    drop_fields = [f.name for f in arcpy.ListFields('WL', zone_field + "_*")]
+    drop_fields = [f.name for f in arcpy.ListFields('WL_Undissolved', zone_field + "_*")]
     for f in drop_fields:
-        arcpy.DeleteField_management('WL', f)
-    arcpy.CopyRows_management('WL', output_table)
+        arcpy.DeleteField_management('WL_Undissolved', f)
+    arcpy.CopyRows_management('WL_Undissolved', output_table)
 
-    for item in ['WL', 'wetlands_fc_checked'] + temp_tables:
+    for item in ['WL_Undissolved', 'wetlands_fc_checked', 'dissolved_wetlands'] + temp_tables:
         try:
             arcpy.Delete_management(item)
         except:
             continue
+    arcpy.ResetEnvironments()
 
 def main():
     zones_fc = arcpy.GetParameterAsText(0)
     zone_field = arcpy.GetParameterAsText(1)
     wetlands_fc = arcpy.GetParameterAsText(2)
     output_table = arcpy.GetParameterAsText(3)
-    wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table)
+    dissolve_wetlands = arcpy.GetParameter(4) # boolean
+    wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table, dissolve_wetlands)
 
 def test():
     ws = 'C:/Users/smithn78/CSI_Processing/CSI/TestData_0411.gdb'
     zones_fc = os.path.join(ws, 'HU12')
     zone_field = 'ZoneID'
     wetlands_fc =  os.path.join(ws, 'Wetlands')
-    output_table = 'C:/GISData/Scratch/Scratch.gdb/test_wetlands_in_zones'
-    wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table)
+    output_table = 'C:/GISData/Scratch/Scratch.gdb/test_wetlands_in_zones2_dissolve'
+    dissolve_wetlands = True
+    wetlands_in_zones(zones_fc, zone_field, wetlands_fc, output_table, dissolve_wetlands)
 
 if __name__ == '__main__':
     main()

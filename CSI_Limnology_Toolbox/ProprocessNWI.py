d7582ce2cda37166f5b8da2651311ac946ed9d6d
nicolejeansmith <nicole.j.smith@gmail.com>
2014-08-29 11:01:58 -0400

    PreprocessNWI.py put everything in functions
    Signed-off-by: nicolejeansmith <nicole.j.smith@gmail.com>

--- CSI_Limnology_Toolbox/ProprocessNWI.py
+++ CSI_Limnology_Toolbox/ProprocessNWI.py
@@ -1,10 +1,6 @@
 # preprocessNWI.py
 
 import arcpy, os
-arcpy.env.overwriteOutput = True
-state = arcpy.GetParameterAsText(0) # state poly
-nwi = arcpy.GetParameterAsText(1) # nwi
-outfc = arcpy.GetParameterAsText(2) # output shapefile
 
 def InsideState(state,nwi,outfc):
     mem = "in_memory"
@@ -13,12 +9,17 @@ def InsideState(state,nwi,outfc):
     arcpy.FeatureClassToFeatureClass_conversion(nwi, mem, "nwi", where)
     arcpy.MakeFeatureLayer_management("nwi", "nwi_lyr", where, mem)
     arcpy.SelectLayerByLocation_management("nwi_lyr", 'HAVE_THEIR_CENTER_IN', state,'','NEW_SELECTION')
-    
+
     arcpy.CopyFeatures_management("nwi_lyr",outfc)
 
-if __name__ == '__main__':
+def main():
+    state = arcpy.GetParameterAsText(0) # state poly
+    nwi = arcpy.GetParameterAsText(1) # nwi
+    outfc = arcpy.GetParameterAsText(2) # output shapefile
     InsideState(state,nwi,outfc)
 
+if __name__ == '__main__':
+    main()
 
 
 

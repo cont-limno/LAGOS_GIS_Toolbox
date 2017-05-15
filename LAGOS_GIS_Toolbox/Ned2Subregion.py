17e28854523ecd49ca2eb69ac79b603652420e6b
Nicole Jean Smith <nicole.j.smith@gmail.com>
2017-05-15 14:41:49 -0400

    Update handling of WBD feature class names to match either version of NHD model

--- LAGOS_GIS_Toolbox/Ned2Subregion.py
+++ LAGOS_GIS_Toolbox/Ned2Subregion.py
@@ -48,7 +48,8 @@ def stage_files(nhd_gdb, ned_dir, ned_footprints_fc, out_dir, is_zipped):
 
     #select only this subregion from the wbd layer in the nhd_gdb (bordering
     # subregions are included in there too) and buffer it
-    wbd_hu4 = os.path.join(nhd_gdb_copy, "WBD_HU4")
+    env.workspace = nhd_gdb_copy
+    wbd_hu4 = arcpy.Describe(arcpy.ListFeatureClasses('*HU4*', feature_dataset = 'WBD')[0]).catalogPath
     field_name = (arcpy.ListFields(wbd_hu4, "HU*4"))[0].name
     whereClause =  """{0} = '{1}'""".format(arcpy.AddFieldDelimiters(nhd_gdb_copy, field_name), huc4_code)
     arcpy.MakeFeatureLayer_management(wbd_hu4, "wbd_poly", whereClause)

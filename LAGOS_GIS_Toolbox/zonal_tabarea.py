dcfd38766e58744c222fa3ace24340d1e674718a
Nicole Jean Smith <nicole.j.smith@gmail.com>
2017-07-24 15:32:39 -0400

    PASSED TEST. Improved handling of NoData rows with overlapping zones

--- LAGOS_GIS_Toolbox/zonal_tabarea.py
+++ LAGOS_GIS_Toolbox/zonal_tabarea.py
@@ -40,7 +40,7 @@ def refine_zonal_output(t, zone_field, is_thematic):
         except:
             continue
 
-def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic):
+def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic, warn_at_end = False):
     orig_env = arcpy.env.workspace
     arcpy.env.workspace = 'in_memory'
     arcpy.CheckOutExtension("Spatial")
@@ -88,25 +88,27 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
     calculate_expr = '100*(1-(float(!COUNT_1!)/!Count!))'
     arcpy.CalculateField_management('zones_VAT', 'Pct_NoData', calculate_expr, "PYTHON")
     refine_zonal_output('zones_VAT', zone_field, is_thematic)
-    arcpy.CopyRows_management('zones_VAT', out_table)
+
+    # final table gets a record even for no-data zones
+    keep_fields = [f.name for f in arcpy.ListFields('zones_VAT')]
+    if zone_field.upper() in keep_fields:
+        keep_fields.remove(zone_field.upper())
+    if zone_field in keep_fields:
+        keep_fields.remove(zone_field)
+    cu.one_in_one_out('zones_VAT', keep_fields, zone_fc, zone_field, out_table)
+
+    # Convert missing "Pct_NoData" values to 100
+    codeblock = """def convert_pct(arg1):
+        if arg1 is None:
+            return float(100)
+        else:
+            return arg1"""
+    arcpy.CalculateField_management(out_table, 'Pct_NoData', 'convert_pct(!Pct_NoData!)', 'PYTHON_9.3', codeblock)
 
     # count whether all zones got an output record or not)
     out_count = int(arcpy.GetCount_management('temp_entire_table').getOutput(0))
     in_count = int(arcpy.GetCount_management(zone_fc).getOutput(0))
     count_diff = in_count - out_count
-    if count_diff > 0:
-        warn_msg = ("WARNING: {0} zones have null zonal statistics. There are 3 possible reasons:\n"
-                    "1) Presence of zones that are fully outside the extent of the raster summarized.\n"
-                    "2) Overlapping zones in the input. Use Subset Overlapping Zones tool, then run again.\n"
-                    "3) Zones are too small relative to the raster resolution.".format(count_diff))
-        arcpy.AddWarning(warn_msg)
-        # Convert missing "Pct_NoData" values to 100
-        codeblock = """def convert_pct(arg1):
-            if arg1 is None:
-                return float(100)
-            else:
-                return arg1"""
-        arcpy.CalculateField_management(out_table, 'Pct_NoData', 'convert_pct(!Pct_NoData!)', 'PYTHON_9.3', codeblock)
 
     # cleanup
     for item in ['temp_zonal_table', 'temp_entire_table', 'convert_raster', 'zones_VAT']:
@@ -119,19 +121,30 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
 
 def handle_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic):
     overlap_grp_field = arcpy.ListFields(zone_fc, 'OVERLAP_GROUP*')
+    total_count_diff = 0
     if overlap_grp_field:
+        arcpy.AddMessage("Calculating zonal stats by overlap avoidance group. Expect repeated messages...")
         groups = list(set([row[0] for row in arcpy.da.SearchCursor(zone_fc, ['OVERLAP_GROUP'])]))
         i_out_tables = []
         for group in groups:
-            i_zone_fc = arcpy.Select_analysis(zone_fc, '"OVERLAP_GROUP" = {}'.format(group))
+            i_zone_fc = 'in_memory/zones{}'.format(group)
+            arcpy.Select_analysis(zone_fc, i_zone_fc, '"OVERLAP_GROUP" = {}'.format(group))
             i_out_table = 'in_memory/stats{}'.format(group)
             i_out_tables.append(i_out_table)
-            stats_area_table(i_zone_fc, zone_field, in_value_raster, i_out_table, is_thematic)
+            result = stats_area_table(i_zone_fc, zone_field, in_value_raster, i_out_table, is_thematic, warn_at_end = True)
+            total_count_diff += result[1]
         arcpy.Merge_management(i_out_tables, out_table)
 
     else:
-        stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic)
+        result = stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic)
+        total_count_diff = result[1]
 
+    if total_count_diff > 0:
+        warn_msg = ("WARNING: {0} zones have null zonal statistics. There are 3 possible reasons:\n"
+                    "1) Presence of zones that are fully outside the extent of the raster summarized.\n"
+                    "2) Overlapping zones in the input. Use Subset Overlapping Zones tool, then run again.\n"
+                    "3) Zones are too small relative to the raster resolution.".format(total_count_diff))
+        arcpy.AddWarning(warn_msg)
 
 def main():
     zone_fc = arcpy.GetParameterAsText(0)
@@ -149,8 +162,7 @@ def test(out_table, is_thematic = False):
     zone_fc = os.path.join(test_data_gdb, 'HU12')
     zone_field = 'ZoneID'
     if is_thematic:
-        #in_value_raster = os.path.join(test_data_gdb, 'NLCD_LandCover_2006')
-        in_value_raster = r'C:\Users\smithn78\Documents\ArcGIS\Default.gdb\NLCD_LandCover_2006_Clip'
+        in_value_raster = os.path.join(test_data_gdb, 'NLCD_LandCover_2006')
     else:
         in_value_raster = os.path.join(test_data_gdb, 'Total_Nitrogen_Deposition_2006')
     handle_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic)
@@ -158,3 +170,4 @@ def test(out_table, is_thematic = False):
 
 if __name__ == '__main__':
     main()
+

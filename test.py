8bbf1c353625cd5076411d8449dfdf0a6c427834
Nicole Jean Smith <nicole.j.smith@gmail.com>
2017-08-10 16:25:38 -0400

    carry debug_mode through test properly

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import lagosGIS
 
 __all__ = ["test_lake_connectivity_classification"]
 
-def lake_connectivity_classification(out_feature_class):
+def lake_connectivity_classification(out_feature_class, debug_mode = True):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     test_data_gdb = os.path.abspath(os.path.join(os.curdir, 'TestData_0411.gdb'))
-    lagosGIS.lake_connectivity_classification(test_data_gdb, out_feature_class)
+    lagosGIS.lake_connectivity_classification(test_data_gdb, out_feature_class, debug_mode)

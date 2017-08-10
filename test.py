4ccd2cfe42a711f74809c11107a786cf80e65f3c
Nicole Jean Smith <nicole.j.smith@gmail.com>
2017-08-10 11:47:44 -0400

    Add first test to test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import lagosGIS
+
+__all__ = ["test_lake_connectivity_classification"]
+
+def lake_connectivity_classification(out_feature_class):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    test_data_gdb = os.path.abspath(os.path.join(os.curdir, 'TestData_0411.gdb'))
+    lagosGIS.lake_connectivity_classification(test_data_gdb, out_feature_class)

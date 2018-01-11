68bf6695e2094fad09953dab6d6bc4c891785a42
Nicole Jean Smith <nicole.j.smith@gmail.com>
2018-01-11 14:09:46 -0500

    Update csv export tool to rename variables
    At this point, I would say that this particular script is very over-loaded, overly-specific, and messily written. It works for the tasks(s) at hand, at least.

--- lagosGIS/Export2CSV.py
+++ lagosGIS/Export2CSV.py
@@ -1,8 +1,81 @@
 # Filename: Export2CSV.py
 import csv, datetime, os, math, re
+from tempfile import NamedTemporaryFile
+import shutil
+import fileinput
 import arcpy
 import csiutils as cu
 from decimal import *
+
+def describe_arcgis_table_csv(in_table, out_path, field_list = [], rename_fields = True):
+    """
+    :param in_table: A feature class or table used by ArcCatalog.
+    :param out_path: Where to save the output. Defaults to the input table named appended with "_cols.csv"
+    :return: None
+    """
+    all_fields = arcpy.ListFields(in_table)
+    if field_list:
+        fields = [f for f in all_fields if f.name in field_list]
+    else:
+        fields = all_fields
+    string_fields = [f.name for f in fields if f.type == 'String']
+    if string_fields:
+        try:
+            arr = arcpy.da.TableToNumpyArray(in_table, string_fields)
+        except:
+            arr = arcpy.da.FeatureClassToNumPyArray(in_table, string_fields)
+
+        #gets the maximum string length for each field and returns a dictionary named with the fields
+        string_lengths = dict(zip(string_fields, [len(max(arr[f], key=len)) for f in string_fields]))
+    else:
+        string_lengths = {}
+
+    with open(out_path, 'wb') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=[
+            'column_name', 'column_esri_type', 'column_esri_length', 'column_string_min'])
+        writer.writeheader()
+
+        for f in fields:
+            if rename_fields: # this code is the duplicated part that I don't like that much.
+                if 'OBJECTID' in f.name:
+                    continue
+                if f.name not in ('ZoneID', 'lagoslakeid'):
+                    short_f_orig = os.path.splitext(os.path.basename(in_table))[0]
+                    short_f = short_f_orig.replace('_QA_ONLY', '')
+                    f.name = '{}_{}'.format(short_f, f.name)
+
+            if f.name in string_lengths:
+                shortest = string_lengths[f.name]
+            else:
+                shortest = ''
+            out_dict = {'column_name': f.name, 'column_esri_type': f.type, 'column_esri_length': f.length, \
+                        'column_string_min': shortest}
+            writer.writerow(out_dict)
+    return(out_path)
+
+def rename_variables(file):
+    tempfile = NamedTemporaryFile(delete=False)
+    short_f_orig = os.path.splitext(os.path.basename(file))[0]
+    short_f = short_f_orig.replace('_QA_ONLY', '')
+
+    with open(file, 'rb') as csv_file:
+        reader = csv.DictReader(csv_file)
+        header = reader.fieldnames
+
+        #update the header
+        desired_header = ['{}_{}'.format(short_f, name) if name not in ('ZoneID', 'lagoslakeid') else name for name in header]
+        update_dict = dict(zip(header, desired_header))
+        filtered_header = [name for name in desired_header if 'OBJECT' not in name]
+
+        # write out selected fields with new names
+        with tempfile:
+            writer = csv.DictWriter(tempfile, fieldnames = filtered_header)
+            writer.writeheader()
+            for row in reader:
+                new_row = {update_dict[k]: row[k] for k in row if update_dict[k] in filtered_header}
+                writer.writerow(new_row)
+
+    shutil.move(tempfile.name, file)
 
 def TableToCSV(in_table, out_folder, output_schema = True, export_qa_version = True, field_list = [], new_table_name = ''):
     if new_table_name:
@@ -78,9 +151,12 @@ def TableToCSV(in_table, out_folder, output_schema = True, export_qa_version = T
                 values = map(format_value, row)
                 f.write(','.join(values)+'\n')
 
+    rename_variables(out_csv)
+    rename_variables(out_qa_csv)
+
     if output_schema:
         out_schema = os.path.join(out_folder, "{}_schema.csv".format(name))
-        cu.describe_arcgis_table_csv(in_table, out_schema, field_list)
+        out_schema = describe_arcgis_table_csv(in_table, out_schema, field_list)
 
 def main():
     in_table = arcpy.GetParameterAsText(0)

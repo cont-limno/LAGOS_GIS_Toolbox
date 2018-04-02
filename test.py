ac93d89358a341be6c4049c1ac1e9210d338e7fc
Nicole Jean Smith <nicole.j.smith@gmail.com>
2018-04-02 14:21:37 -0400

    Add zonal_attribution_of_raster_data TEST

--- test.py
+++ test.py
@@ -9,3 +9,11 @@ def lake_connectivity_classification(out_feature_class, debug_mode = True):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     test_data_gdb = os.path.abspath(os.path.join(os.curdir, 'TestData_0411.gdb'))
     lagosGIS.lake_connectivity_classification(test_data_gdb, out_feature_class, debug_mode)
+
+def zonal_attribution_of_raster_data(out_table, debug_mode = True):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    test_data_gdb = os.path.abspath(os.path.join(os.curdir, 'TestData_0411.gdb'))
+    zone_fc = os.path.join(test_data_gdb, 'HU12')
+    in_value_raster = os.path.join('Total_Nitrogen_Deposition_2006')
+    lagosGIS.zonal_attribution_of_raster_data(zone_fc, 'ZoneID', in_value_raster,
+                                              out_table, is_thematic = False, debug_mode =  debug_mode)

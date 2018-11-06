0321ff92c0198775e4a12fafc146eae8b5668b2f
Nicole Jean Smith <nicole.j.smith@gmail.com>
2018-11-06 17:40:28 -0500

    Add progress counter and improved table handling to lake_from_to

--- test_network_only.py
+++ test_network_only.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import arcpy
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -13,4 +14,7 @@ __all__ = ['lake_from_to']
 test_data_gdb = r'D:\Continental_Limnology\Data_Downloaded\National_Hydrography_Dataset\Unzipped_Original\NHD_H_0405_GDB.gdb'
 
 def lake_from_to(output_table):
+    start = time.time()
     lagosGIS.lake_from_to(test_data_gdb, output_table)
+    end = time.time()
+    print "Time to complete: {}".format(end - start)

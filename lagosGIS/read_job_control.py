56fd5824d3145b51ec12c74aa17ae1cd991956af
Nicole Jean Smith <nicole.j.smith@gmail.com>
2018-12-06 14:06:38 -0500

    add csv export to job control file reader

--- lagosGIS/read_job_control.py
+++ lagosGIS/read_job_control.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 import arcpy
 import lagosGIS
 
@@ -16,13 +17,15 @@ def read_job_control(job_control_csv, start_line = -1, end_line = -1):
             lines = lines[start_line-1:end_line]
 
     calls = []
-
+    outputs = []
+    csv_paths = []
     def cook_string(input):
         if input.startswith('r\'') and input.endswith('\''):
             return input[2:-1]
         else:
             return input
 
+    # Read the table and compose the calls
     for line in lines:
         function = cook_string(line['Function'])
         arg1 = cook_string(line['Arg1'])
@@ -30,6 +33,9 @@ def read_job_control(job_control_csv, start_line = -1, end_line = -1):
         arg3 = cook_string(line['Arg3'])
         arg4 = cook_string(line['Arg4'])
         output = cook_string(line['Output'])
+        csv_path = cook_string(line['CSV'])
+        outputs.append(output)
+        csv_paths.append(csv_path)
         calls.append("lagosGIS.{f}(r'{a1}', r'{a2}', r'{a3}', r'{a4}')".format(
             f = function,
             a1 = arg1,
@@ -37,11 +43,20 @@ def read_job_control(job_control_csv, start_line = -1, end_line = -1):
             a3 = arg3,
             a4 = arg4))
 
-    for call in calls:
+    # Call each tool and export the result to CSV
+    for call, output, csv_path in zip(calls, outputs, csv_paths):
         output_dir = os.path.dirname(output)
         if not arcpy.Exists(output_dir):
             raise Exception("Provide a valid geodatabase for the output.")
         if not arcpy.Exists(output):
+            print time.ctime()
             print(call)
             eval(call)
+
+            out_folder = os.path.dirname(csv_path)
+            out_basename = os.path.splitext(os.path.basename(csv_path))[0]
+            lagosGIS.export_to_csv(output, out_folder, new_table_name=out_basename)
+
+        # Keep in_memory workspace from carrying over to the next call
+        arcpy.Delete_management('in_memory')
 

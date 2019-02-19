be683cbc7dc8267b6429c54b709f9a1e61012961
Nicole Jean Smith <nicole.j.smith@gmail.com>
2019-02-19 15:00:04 -0500

    initiate nhdplus hr tools

---
+++ lagosGIS/nhdplushr_tools.py
@@ -0,0 +1,122 @@
+import os
+from arcpy import management as DM
+import arcpy
+
+def assign_catchments_to_lakes(nhdplus_gdb, output_fc):
+    # paths
+    nhd_cat = os.path.join(nhdplus_gdb, 'NHDPlusCatchment')
+    nhd_flowline = os.path.join(nhdplus_gdb, 'NHDFlowline')
+    nhd_wb = os.path.join(nhdplus_gdb, 'NHDWaterbody')
+
+    # copy to output and prep
+    nhd_cat_copy = DM.CopyFeatures(nhd_cat, output_fc)
+    DM.AddField(nhd_cat_copy, 'Lake_Permanent_Identifier', field_type = 'TEXT', field_length = 40)
+
+    # build dictionaries for the joins
+    nhd_flowline_dict = {r[0]:r[1] for r in arcpy.da.SearchCursor(nhd_flowline,
+                                                                   ['NHDPlusID', 'WBArea_Permanent_Identifier'])}
+    nhd_wb_dict = {r[0]:r[1] for r in arcpy.da.SearchCursor(nhd_wb, ['NHDPlusID', 'Permanent_Identifier'])}
+    valid_wb_ids = set(nhd_wb_dict.values())
+    # some WBArea_... values come from NHDArea polygons, not NHDWaterbody. Filter dictionary for valid only.
+    flowline_wb_dict = {nhdplusid:nhd_flowline_dict[nhdplusid] for nhdplusid, wb_permid in nhd_flowline_dict.items() if wb_permid in valid_wb_ids}
+
+    with arcpy.da.UpdateCursor(nhd_cat_copy, ['NHDPlusID', 'Lake_Permanent_Identifier']) as nhd_cat_copy_cursor:
+
+        # use UpdateCursor to "join" to get waterbody ids
+        for u_row in nhd_cat_copy_cursor:
+            nhdplusid = u_row[0]
+            lake_permid_flowline = None
+            lake_permid_sink = None
+
+            # like SELECT WBArea_Permanent_Identifier FROM nhd_cat LEFT JOIN nhd_flowline
+            if nhdplusid in flowline_wb_dict:
+                lake_permid_flowline = flowline_wb_dict[nhdplusid]
+
+            # join to sinks to get sink to lake mapping
+            # like SELECT Permanent_Identifier FROM nhd_cat LEFT JOIN nhd_wb
+            if nhdplusid in nhd_wb_dict:
+                lake_permid_sink = nhd_wb_dict[nhdplusid]
+
+            # concatenate & calculate update
+            if lake_permid_flowline:
+                lake_permid = lake_permid_flowline
+            else:
+                lake_permid = lake_permid_sink
+
+            # write the update
+            new_row = (nhdplusid, lake_permid)
+            nhd_cat_copy_cursor.updateRow(new_row)
+
+    return nhd_cat_copy
+
+def merge_lake_catchments(nhdplus_gdb, output_catchments_fc):
+    arcpy.env.workspace = 'in_memory'
+    catchments_assigned = assign_catchments_to_lakes(nhdplus_gdb, 'catchments_assigned')
+
+    # set NHDPlusID to null if lake ID is populated, so that we can dissolve all lake catchments into one
+    # but keep the distinct flowline segment catchments also
+    with arcpy.da.UpdateCursor(catchments_assigned, ['NHDPlusID', 'Lake_Permanent_Identifier']) as u_cursor:
+        for row in u_cursor:
+            if row[1]:
+                row[0] = None
+            u_cursor.updateRow(row)
+
+    output_fc = DM.Dissolve(catchments_assigned, output_catchments_fc, 'Lake_Permanent_Identifier')
+    arcpy.Delete_management(catchments_assigned)
+    return output_fc
+
+
+def aggregate_lake_watershed(nhdplus_gdb, lake_catchments_fc, output_fc):
+    pass
+    # expecting to re-use methods from LAGOS-NE but with maybe something different out of the IDs?
+
+
+def calculate_waterbody_strahler(nhdplus_gdb, output_table):
+    nhd_wb = os.path.join(nhdplus_gdb, 'NHDWaterbody')
+    nhd_flowline = os.path.join(nhdplus_gdb, 'NHDFlowline')
+    nhd_vaa = os.path.join(nhdplus_gdb, 'NHDPlusFlowlineVAA')
+
+    # build dictionaries for the joins
+    nhd_wb_dict = {r[0]: r[1] for r in arcpy.da.SearchCursor(nhd_wb, ['Permanent_Identifier', 'NHDPlusID'])}
+    # filter upcoming dictionary to only include artificial flowlines (but with both waterbody and NHDArea ids)
+    nhd_flowline_dict = {r[0]: r[1] for r in arcpy.da.SearchCursor(nhd_flowline,
+                                                ['NHDPlusID', 'WBArea_Permanent_Identifier']) if r[1]}
+    # filter out flowlines we can't get strahler for NOW, so that loop below doesn't have to test
+    nhd_vaa_dict = {r[0]: r[1] for r in arcpy.da.SearchCursor(nhd_vaa, ['NHDPlusID', 'StreamOrde'])
+                    if r[0] in nhd_flowline_dict}
+    # filter out lines with uninitialized flow as they don't appear in nhd_vaa_dict
+    nhd_flowline_dict2 = {key:val for key,val in nhd_flowline_dict.items() if key in nhd_vaa_dict}
+
+    # set up new table
+    output_table = DM.CreateTable(os.path.dirname(output_table), os.path.basename(output_table))
+    DM.AddField(output_table, 'Lake_Permanent_Identifier', 'TEXT', field_length=40)
+    DM.AddField(output_table, 'NHDPlusID', 'DOUBLE')
+    DM.AddField(output_table, 'lake_higheststrahler', 'SHORT')
+    DM.AddField(output_table, 'lake_loweststrahler', 'SHORT')
+
+    # set up cursor
+    out_rows = arcpy.da.InsertCursor(output_table, ['Lake_Permanent_Identifier',
+                                                    'NHDPlusID',
+                                                    'lake_higheststrahler',
+                                                    'lake_loweststrahler'])
+
+    # function to call for each waterbody
+    nhd_flowline_dict_items = nhd_flowline_dict2.items()
+    def get_matching_strahlers(wb_permid):
+        matching_strahlers = [nhd_vaa_dict[flow_plusid] for flow_plusid, linked_wb_permid
+                              in nhd_flowline_dict_items if linked_wb_permid == wb_permid]
+        return matching_strahlers
+
+    # populate the table with the cursor
+    for wb_permid, nhdplusid in nhd_wb_dict.items():
+        strahlers = get_matching_strahlers(wb_permid)
+        if strahlers:
+            new_row = (wb_permid, nhdplusid, max(strahlers), min(strahlers))
+        else:
+            new_row = (wb_permid, nhdplusid, None, None)
+        out_rows.insertRow(new_row)
+
+    del out_rows
+
+
+

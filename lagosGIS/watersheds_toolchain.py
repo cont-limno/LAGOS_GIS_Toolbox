5bb961ba1d4e85037ea442da2ba769452ccbfc1b
Nicole Jean Smith <nicole.j.smith@gmail.com>
2019-10-18 16:53:01 -0400

    messy commit oct 18

--- lagosGIS/watersheds_toolchain.py
+++ lagosGIS/watersheds_toolchain.py
@@ -16,7 +16,7 @@ ALT_TOOL_ORDER = ('mosaic_dem', 'burn_dem', 'make_hydrodem', 'make_catseed', 'fd
 # Locations of main directories (unique to machine)
 
 # this is the result of the lakes_in_the_us/doit.py
-LAGOS_LAKES = r'D:\Continental_Limnology\Data_Working\LAGOS_US_GIS_Data_v0.5.gdb\Lakes\LAGOS_US_All_Lakes_1ha'
+LAGOS_LAKES = r'D:\Continental_Limnology\Data_Working\LAGOS_US_GIS_Data_v0.6.gdb\Lakes\LAGOS_US_All_Lakes_1ha'
 HU4 = r'D:\Continental_Limnology\Data_Working\LAGOS_US_GIS_Data_v0.5.gdb\Spatial_Classifications\hu4'
 
 # straight as downloaded from web in mid-March 2019
@@ -45,6 +45,7 @@ class Paths:
     :param str huc4: 4-digit HUC4 for the geodatabase to be processed.
 
     """
+
     def __init__(self, huc4, hr=True):
         self.huc4 = huc4
         self.hr = hr
@@ -62,23 +63,25 @@ class Paths:
             self.catseed = path.join(self.rasters_dir, 'catseed.tif')
             self.fdr = path.join(self.rasters_dir, 'fdr.tif')
         self.waterbody = path.join(self.gdb, 'NHDWaterbody')
-
-
         # output items that don't exist at start
         self.out_dir = path.join(OUTPUTS_PARENT_DIR, 'watersheds_{}'.format(huc4))
-        self.out_gdb = path.join(self.out_dir, 'watersheds_{}.gdb'.format(huc4))
-        self.lagos_gridcode = path.join(self.out_gdb, 'lagos_gridcode_{}'.format(huc4))
-        self.lagos_catseed = path.join(self.out_dir, 'lagos_catseed_{}.tif'.format(huc4))
-        self.lagos_burn = path.join(self.out_dir, 'lagos_burn_{}.tif'.format(huc4))
-        self.lagos_fel = path.join(self.out_dir, 'lagos_hydrodem_{}_fel.tif'.format(huc4))
-        self.lagos_fdr = path.join(self.out_dir, 'lagos_fdr_{}.tif'.format(huc4))
-        self.local_catchments = path.join(self.out_gdb, 'lagos_catchments_{}'.format(huc4))
-        self.sheds_base = path.join(self.out_gdb, 'lagos_watersheds_{}'.format(huc4))
-        self.iws_sheds = path.join(self.out_gdb, 'lagos_watersheds_{}_interlake'.format(huc4))
-        self.network_sheds = path.join(self.out_gdb, 'lagos_watersheds_{}_network'.format(huc4))
+        self.locate_outputs()
+
+    def locate_outputs(self):
+        self.out_gdb = path.join(self.out_dir, 'watersheds_{}.gdb'.format(self.huc4))
+        self.lagos_gridcode = path.join(self.out_gdb, 'lagos_gridcode_{}'.format(self.huc4))
+        self.lagos_catseed = path.join(self.out_dir, 'lagos_catseed_{}.tif'.format(self.huc4))
+        self.lagos_burn = path.join(self.out_dir, 'lagos_burn_{}.tif'.format(self.huc4))
+        self.lagos_walled = path.join(self.out_dir, 'lagos_burn_{}_walled.tif'.format(self.huc4))
+        self.lagos_fel = path.join(self.out_dir, 'lagos_hydrodem_{}_fel.tif'.format(self.huc4))
+        self.lagos_fdr = path.join(self.out_dir, 'lagos_fdr_{}.tif'.format(self.huc4))
+        self.local_catchments = path.join(self.out_gdb, 'lagos_catchments_{}'.format(self.huc4))
+        self.sheds_base = path.join(self.out_gdb, 'lagos_watersheds_{}'.format(self.huc4))
+        self.iws_sheds = path.join(self.out_gdb, 'lagos_watersheds_{}_interlake'.format(self.huc4))
+        self.network_sheds = path.join(self.out_gdb, 'lagos_watersheds_{}_network'.format(self.huc4))
 
         # alternate workflow only
-        self.dem = path.join(self.out_dir,'NED13_{}.tif'.format(huc4))
+        self.dem = path.join(self.out_dir,'NED13_{}.tif'.format(self.huc4))
 
     def exist(self):
         """Check whether NHDPlus data available locally in order to proceed."""
@@ -133,25 +136,25 @@ class Paths:
         with open(file, 'a') as file:
             file.write(line)
 
-    def photograph(self, overwrite=False):
-        if not path.exists(self.out_dir):
-            os.mkdir(self.out_dir)
-        hmin = float(arcpy.GetRasterProperties_management(self.hydrodem, 'MINIMUM').getOutput(0))
-        hmax = float(arcpy.GetRasterProperties_management(self.hydrodem, 'MAXIMUM').getOutput(0)) - 500000
-
-        vals = {self.catseed:[0, 1, -32768, 50, 50],
-                self.fdr: [0, 128, 255, 25, 25],
-                self.hydrodem: [hmin, hmax, -2147483648, 50, 50],
-                self.lagos_catseed: [0, 1, -32768, 50, 50],
-                self.lagos_fdr: [0, 128, 255, 25, 25],
-                self.lagos_fel: [hmin, hmax, -2147483648, 50, 50]
-                }
-        for tif, values in vals.items():
-            jpg = path.join(self.out_dir, '{}_{}.jpg'.format(path.splitext(path.basename(tif))[0], self.huc4))
-            if overwrite or not path.exists(jpg):
-                gdal_cmd = 'gdal_translate -a_nodata {} -of JPEG -co worldfile=yes -b 1 -b 1 -b 1 -scale {} {} 0 255 -outsize {}% {}% {} {}'.\
-                    format(values[2], values[0], values[1], values[3], values[4], tif, jpg)
-                sp.call(gdal_cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
+    # def photograph(self, overwrite=False):
+    #     if not path.exists(self.out_dir):
+    #         os.mkdir(self.out_dir)
+    #     hmin = float(arcpy.GetRasterProperties_management(self.hydrodem, 'MINIMUM').getOutput(0))
+    #     hmax = float(arcpy.GetRasterProperties_management(self.hydrodem, 'MAXIMUM').getOutput(0)) - 500000
+    #
+    #     vals = {self.catseed:[0, 1, -32768, 50, 50],
+    #             self.fdr: [0, 128, 255, 25, 25],
+    #             self.hydrodem: [hmin, hmax, -2147483648, 50, 50],
+    #             self.lagos_catseed: [0, 1, -32768, 50, 50],
+    #             self.lagos_fdr: [0, 128, 255, 25, 25],
+    #             self.lagos_fel: [hmin, hmax, -2147483648, 50, 50]
+    #             }
+    #     for tif, values in vals.items():
+    #         jpg = path.join(self.out_dir, '{}_{}.jpg'.format(path.splitext(path.basename(tif))[0], self.huc4))
+    #         if overwrite or not path.exists(jpg):
+    #             gdal_cmd = 'gdal_translate -a_nodata {} -of JPEG -co worldfile=yes -b 1 -b 1 -b 1 -scale {} {} 0 255 -outsize {}% {}% {} {}'.\
+    #                 format(values[2], values[0], values[1], values[3], values[4], tif, jpg)
+    #             sp.call(gdal_cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
 
 
 def run(huc4, last_tool='network', wait = False, burn_override=True):
@@ -198,7 +201,10 @@ def run(huc4, last_tool='network', wait = False, burn_override=True):
         tool_count += 1
 
     # fix_hydrodem
-    if not arcpy.Exists(paths.lagos_burn) and stop_index >= 2 and not burn_override:
+    if not arcpy.Exists(paths.lagos_burn) \
+            and stop_index >= 2 \
+            and not arcpy.Exists(paths.network_sheds)\
+            and not burn_override:
         arcpy.AddMessage('Fixing hydrodem burn started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
         nt.fix_hydrodem(paths.hydrodem, paths.lagos_catseed, paths.lagos_burn)
         tool_count += 1
@@ -295,15 +301,13 @@ def make_run_list(master_HU4):
 if __name__ == '__main__':
     run_list = make_run_list(HU4)
     log_file = r"D:\Continental_Limnology\Data_Working\Tool_Execution\Watersheds\watersheds_log.csv"
-    for huc4 in run_list[36:]:
+    for huc4 in run_list:
         p = Paths(huc4)
         try:
             last_tool = 'network'
             tool_count = run(p.huc4, last_tool)
             if tool_count > 0:
                 p.log(log_file, '{}: SUCCESS'.format(last_tool))
-                p.photograph(overwrite=True)
-            p.photograph()
         except Exception as e:
             p.log(log_file, repr(e))
             print(e)
@@ -352,7 +356,8 @@ def patch_on_network_flag():
 
 
 
-def run_alternate(huc4, last_tool='network', wait=False):
+def run_alternate(huc4, last_tool='network', wait=False, out_dir = '', burn_override=True):
+    arcpy.env.overwriteOutput = True
     import Ned2Subregion as mosaic
     from burn_streams import burn_streams
     from WallsHU8 import wall as add_walls
@@ -362,6 +367,9 @@ def run_alternate(huc4, last_tool='network', wait=False):
     NED_FOOTPRINT = r'D:\Continental_Limnology\Data_Downloaded\3DEP_National_Elevation_Dataset\Unzipped Original\ned_13arcsec_g.shp'
 
     paths = Paths(huc4, hr=False)
+    if out_dir:
+        paths.out_dir = out_dir
+        paths.locate_outputs()
     arcpy.AddMessage("Starting subregion {}...".format(paths.huc4))
     if last_tool:
         stop_index = ALT_TOOL_ORDER.index(last_tool)
@@ -385,25 +393,23 @@ def run_alternate(huc4, last_tool='network', wait=False):
     #TODO: Remove any originals, NED but maybe also NHD, did we modify that at all?
 
     # Create hydrodem burn
-    if not arcpy.Exists(paths.lagos_burn) and stop_index >= 1:
+    if not arcpy.Exists(paths.lagos_burn) and stop_index >= 1 and not burn_override:
         arcpy.AddMessage('Burning DEM started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
-        burn_temp = burn_streams(ned, paths.gdb, paths.lagos_burn) #TODO: Modify tool to protect lakes from fill
+        burn_temp = burn_streams(paths.dem, paths.gdb, paths.lagos_burn) #TODO: Modify tool to protect lakes from fill
         tool_count += 1
 
     # Fill and wall hydrodem
     if not arcpy.Exists(paths.lagos_fel) and stop_index >= 2:
-        arcpy.AddMessage('Filling DEM and adding walls started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+        arcpy.AddMessage('Filling DEM started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
         pitremove_cmd = 'mpiexec -n 8 pitremove -z {} -fel {}'.format(paths.lagos_burn, paths.lagos_fel)
+        print(pitremove_cmd)
         sp.call(pitremove_cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
-        walled = add_walls(paths.gdb, [paths.lagos_fel], paths.out_dir) #TODO: Modify to HU12 walls
-        # rename "wfel" output of add_walls to "fel", didn't need to save intermediate.
-        walled_tif_files = [os.path.join(paths.out_dir,f) for f in os.listdir(paths.out_dir) if 'wfel' in f]
-        os.remove(paths.lagos_fel)
-        for f in walled_tif_files:
-            os.rename(f, f.replace('wfel', 'fel'))
         tool_count += 1
 
     # Create catseed
+    if not arcpy.Exists(paths.lagos_gridcode) and stop_index >=3:
+        nt.make_gridcode(paths.gdb, paths.lagos_gridcode)
+
     paths.lagos_catseed = path.join(paths.out_dir, 'pourpoints{}'.format(huc4), 'lagos_catseed.tif')
     if not arcpy.Exists(paths.lagos_catseed) and stop_index >= 3:
         arcpy.AddMessage('Identifying pour points started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
@@ -427,9 +433,6 @@ def run_alternate(huc4, last_tool='network', wait=False):
 
 
     # delineate catchments
-    if not arcpy.Exists(paths.lagos_gridcode) and stop_index >=5:
-        nt.make_gridcode(paths.gdb, paths.lagos_gridcode)
-
     if not arcpy.Exists(paths.local_catchments) and stop_index >= 5:
         arcpy.AddMessage('Delineating catchments started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
         nt.delineate_catchments(paths.lagos_fdr, paths.lagos_catseed, paths.gdb, paths.lagos_gridcode, paths.local_catchments)
@@ -463,4 +466,155 @@ def run_alternate(huc4, last_tool='network', wait=False):
             'Network watersheds started at {}...'.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
         nt.aggregate_watersheds(paths.local_catchments, paths.gdb, LAGOS_LAKES, paths.network_sheds, 'network')
         tool_count += 1
+    return tool_count
+
+def make_alt_run_list(master_HU4):
+    """Make a run list that will output one huc4 from most regions first, for QA purposes."""
+    regions = ['{:02d}'.format(i) for i in range(1,19)]
+    subregions = ['{:02d}'.format(i) for i in range(1,31)]
+    template = []
+
+    # do first 2 from each region first
+    for s in subregions[:2]:
+        template.extend(['{}{}'.format(r, s) for r in regions])
+    # then go by region after that
+    for r in regions:
+        template.extend(['{}{}'.format(r, s) for s in subregions[2:]])
+
+    huc4 = [r[0] for r in arcpy.da.SearchCursor(master_HU4, 'hu4_huc4')]
+
+    # Select only the ones that don't have HR Plus data
+    no_plus = [i for i in template if i in huc4 and i[:2] in ['04', '08', '18']]
+    no_plus.remove('1806')
+    no_plus.remove('1807')
+    no_plus.remove('1808')
+    no_plus.remove('1809')
+    no_plus.remove('1810')
+    no_plus.remove('1801')
+
+    # Add regions that need alt-processing along with regular
+    no_plus.extend(['0512', '0712', '1111', '0508'])
+    no_plus.sort()
+
+    return no_plus
+
+def run_alt_batch():
+    run_list = make_alt_run_list(HU4)
+    log_file = r"D:\Continental_Limnology\Data_Working\Tool_Execution\Watersheds\watersheds_log.csv"
+    for huc4 in run_list:
+        p = Paths(huc4)
+        try:
+            last_tool = 'network'
+            if huc4 in ['0512', '0712', '1111', '0508']:
+                tool_count = run_alternate(p.huc4, last_tool, out_dir = p.out_dir + '_alt')
+            else:
+                tool_count = run_alternate(p.huc4, last_tool)
+            if tool_count > 0:
+                p.log(log_file, '{}: SUCCESS'.format(last_tool))
+        except Exception as e:
+            p.log(log_file, repr(e))
+            print(e)
+        finally:
+            arcpy.Delete_management('in_memory')
+
+def add_ws_flags():
+    # run_list = make_run_list(HU4)
+    run_list  = make_run_list(HU4)
+    failures = []
+    for huc4 in run_list:
+        print(huc4)
+        p = Paths(huc4)
+        nt.watershed_equality(p.iws_sheds, p.network_sheds)
+        nt.qa_shape_metrics(p.iws_sheds, p.network_sheds, LAGOS_LAKES)
+
+
+def add_hr_indicator():
+    not_hr = ['0401', '0402', '0403', '0404', '0405', '0406', '0407',
+              '0408', '0409', '0410', '0411', '0412', '0413', '0414',
+              '0415', '0801', '0802', '0803',
+              '0804', '0805', '0806', '0807', '0808', '0809',
+              '1802', '1803', '1804', '1805']
+    both = ['0512', '0712', '1111', '0508']
+    hr = [h for h in make_run_list(HU4) if h not in not_hr and h not in both] + both
+    p1 = [Paths(h) for h in not_hr]
+    p2 = [Paths(h) for h in both]
+    for p in p2:
+        p.out_dir = p.out_dir + '_alt'
+        p.locate_outputs()
+    non_hr_paths = p1 + p2
+    hr_paths = [Paths(h) for h in hr]
+
+    for p in non_hr_paths:
+        print(p.huc4)
+        fcs = [p.local_catchments, p.iws_sheds, p.network_sheds]
+        for fc in fcs:
+            if not arcpy.ListFields(fc, 'VPUID'):
+                arcpy.AddField_management(fc, 'watershedprocess', 'TEXT', field_length=20)
+                if fc != p.local_catchments:
+                    arcpy.AddField_management(fc, 'VPUID', 'TEXT', field_length=4)
+                with arcpy.da.UpdateCursor(fc, ['VPUID', 'watershedprocess']) as cursor:
+                    for row in cursor:
+                        cursor.updateRow((p.huc4, 'NHD HR 2017'))
+
+    for p in hr_paths:
+        print(p.huc4)
+        fcs = [p.local_catchments, p.iws_sheds, p.network_sheds]
+        for fc in fcs:
+            if not arcpy.ListFields(fc, 'VPUID'):
+                arcpy.AddField_management(fc, 'watershedprocess', 'TEXT', field_length=20)
+                if fc != p.local_catchments:
+                    arcpy.AddField_management(fc, 'VPUID', 'TEXT', field_length=4)
+                with arcpy.da.UpdateCursor(fc, ['VPUID', 'watershedprocess']) as cursor:
+                    for row in cursor:
+                        cursor.updateRow((p.huc4, 'NHDPlus HR Beta 2019'))
+
+
+def merge_watersheds(parent_directory, output_fc, tag):
+    merge_fcs = []
+    alt_merge_fcs = []
+    walk = arcpy.da.Walk(parent_directory, datatype='FeatureClass')
+    for dirpath, dirnames, filenames in walk:
+        for filename in filenames:
+            if tag in filename and 'alt' not in dirpath:
+                merge_fcs.append(os.path.join(dirpath, filename))
+            if tag in filename and 'alt' in dirpath and tag == 'catchment':
+                alt_merge_fcs.append(os.path.join(dirpath, filename))
+
+    # merge the main results
+    if tag == 'catchment' or tag == 'interlake':
+        output_fc = lagosGIS.efficient_merge(merge_fcs, output_fc)
+
+    elif tag == 'network':
+        output_fc = lagosGIS.efficient_merge(merge_fcs, output_fc, "equalsiws = 'N'")
+
+    # fill in some of the regions where the NHDPlus HR falls short of the HU4 boundary we used along a border where
+    # NHDPlus is used on one side and NHD is used on the other
+
+    # These regions cannot be in any lake's watershed but they allow the landscape to be dissected
+    if tag == 'catchment':
+        MERGE_REGIONS = r'D:\Continental_Limnology\Data_Working\LAGOS_US_GIS_Data_v0.6.gdb\NHD_Combined_Regions'
+        for fc in alt_merge_fcs:
+            lyr = arcpy.MakeFeatureLayer_management(fc, 'lyr')
+            arcpy.SelectLayerByLocation_management(lyr, 'INTERSECT', MERGE_REGIONS)
+            arcpy.Append_management(output_fc, lyr)
+
+    return output_fc
+
+def add_subtypes(interlake_fc):
+    not_hr = ['0401', '0402', '0403', '0404', '0405', '0406', '0407',
+              '0408', '0409', '0410', '0411', '0412', '0413', '0414',
+              '0415', '0801', '0802', '0803',
+              '0804', '0805', '0806', '0807', '0808', '0809',
+              '1802', '1803', '1804', '1805']
+    hr = [h for h in make_run_list(HU4) if h not in not_hr]
+
+    for huc4 in hr:
+        print(huc4)
+        p = Paths(huc4)
+        nt.calc_subtype_flag(p.gdb, interlake_fc)
+    for huc4 in not_hr:
+        print(huc4)
+        p = Paths(huc4, hr=False)
+        nt.calc_subtype_flag(p.gdb, interlake_fc)
+
 

0247274b334ff4af490f3f5394bc60349e270ee4
Nicole Jean Smith <nicole.j.smith@gmail.com>
2019-10-28 14:00:43 -0400

    update overlap handling to use "flattening" method (self-union of zones, run stats, re-construct)

--- lagosGIS/zonal_tabarea.py
+++ lagosGIS/zonal_tabarea.py
@@ -1,10 +1,11 @@
-import datetime
 import os
-import tempfile
-import xml.etree.ElementTree as ET
 import arcpy
+from arcpy import management as DM
+from arcpy import analysis as AN
 from arcpy import env
 import csiutils as cu
+import lagosGIS
+from collections import defaultdict
 
 def refine_zonal_output(t, zone_field, is_thematic, debug_mode = False):
     """Makes a nicer output for this tool. Rename some fields, drop unwanted
@@ -78,7 +79,7 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
 
     # I tested and there is no need to resample the raster being summarized. It will be resampled correctly
     # internally in the following tool given that the necessary environments are set above (cell size, snap).
-    #in_value_raster = arcpy.Resample_management(in_value_raster, 'in_value_raster_resampled', CELL_SIZE)
+    # # in_value_raster = arcpy.Resample_management(in_value_raster, 'in_value_raster_resampled', CELL_SIZE)
     if not is_thematic:
         arcpy.AddMessage("Calculating Zonal Statistics...")
         temp_entire_table = arcpy.sa.ZonalStatisticsAsTable(zone_raster, zone_field, in_value_raster, 'temp_zonal_table', 'DATA', 'MIN_MAX_MEAN')
@@ -155,7 +156,7 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
 
     # cleanup
     if not debug_mode:
-        for item in ['temp_zonal_table', 'temp_entire_table', 'in_memory', 'zones_VAT']:
+        for item in ['temp_zonal_table', 'convertraster']:
             arcpy.Delete_management(item)
     arcpy.ResetEnvironments()
     arcpy.env.workspace = orig_env # hope this prevents problems using list of FCs from workspace as batch
@@ -163,55 +164,120 @@ def stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_themati
 
     return [out_table, count_diff]
 
-def handle_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic, debug_mode = False):
-    # TODO: Add in debug_mode feature for this wrapper (for overlaps)
-    overlap_grp_field = arcpy.ListFields(zone_fc, 'OVERLAP_GROUP*')
-    total_count_diff = 0
-    if overlap_grp_field:
-        arcpy.AddMessage("Calculating zonal stats by overlap avoidance group. Expect repeated messages...")
-        groups = list(set([row[0] for row in arcpy.da.SearchCursor(zone_fc, ['OVERLAP_GROUP'])]))
-        i_out_tables = []
-        for group in groups:
-            i_zone_fc = 'in_memory/zones{}'.format(group)
-            arcpy.Select_analysis(zone_fc, i_zone_fc, '"OVERLAP_GROUP" = {}'.format(group))
-            i_out_table = 'in_memory/stats{}'.format(group)
-            i_out_tables.append(i_out_table)
-            result = stats_area_table(i_zone_fc, zone_field, in_value_raster, i_out_table, is_thematic, debug_mode)
-            total_count_diff += result[1]
-        arcpy.Merge_management(i_out_tables, out_table)
+def flatten_overlaps(fc_with_overlapping_polygons, zone_id, in_value_raster, out_table, is_thematic):
+    orig_workspace = arcpy.env.workspace
+    arcpy.env.workspace = 'in_memory'
+    objectid = [f.name for f in arcpy.ListFields(fc_with_overlapping_polygons) if f.type == 'OID'][0]
+    zone_type = [f.type for f in arcpy.ListFields(fc_with_overlapping_polygons, zone_id)][0]
+    fid1 = 'FID_{}'.format(os.path.basename(fc_with_overlapping_polygons))
+    fid2 = fid1 + '_1'
+    flat_zoneid = 'flat{}_zoneid'.format(os.path.basename(fc_with_overlapping_polygons))
+    flat_zoneid_prefix = 'flat{}_'.format(os.path.basename(fc_with_overlapping_polygons))
 
+    # Set up the output table
+    if os.path.dirname(out_table):
+        out_path = os.path.dirname(out_table)
     else:
-        result = stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic, debug_mode)
-        total_count_diff = result[1]
+        out_path = orig_workspace
+
+    result = DM.CreateTable(out_path, os.path.basename(out_table))
+    DM.AddField(result, zone_id, zone_type)
+    DM.AddField(result, 'CELL_COUNT', 'LONG')
+    DM.AddField(result, 'MEAN', 'DOUBLE')
+    DM.AddField(result, 'DataCoverage_pct', 'DOUBLE')
+
+    # Union with FID_Only (A)
+    zoneid_dict = {r[0]:r[1] for r in arcpy.da.SearchCursor(fc_with_overlapping_polygons, [objectid, zone_id])}
+    self_union = AN.Union([fc_with_overlapping_polygons, fc_with_overlapping_polygons], 'self_union', 'ONLY_FID')
+
+    # Add the original zone ids and save to table (E)
+    unflat_table = DM.CopyRows(self_union, 'unflat_table')
+    DM.AddField(unflat_table, zone_id, zone_type) # default text length of 50 is fine if needed
+    with arcpy.da.UpdateCursor(unflat_table, [fid1, zone_id]) as u_cursor:
+        for row in u_cursor:
+            row[1] = zoneid_dict[row[0]] # assign zone id
+            u_cursor.updateRow(row)
+
+    # Find Identical by Shape (B)
+    identical_shapes = DM.FindIdentical(self_union, 'identical_shapes', 'Shape')
+
+    # Join A to B and calc flat[zone]_zoneid = FEAT_SEQ (C)
+    DM.AddField(self_union, flat_zoneid, 'TEXT', field_length=20)
+    identical_shapes_dict = {r[0]:r[1] for r in arcpy.da.SearchCursor(identical_shapes, ['IN_FID', 'FEAT_SEQ'])}
+    with arcpy.da.UpdateCursor(self_union, [objectid, flat_zoneid]) as u_cursor:
+        for row in u_cursor:
+            row[1] = '{}{}'.format(flat_zoneid_prefix, identical_shapes_dict[row[0]])
+            u_cursor.updateRow(row)
+
+    # Add the original zone ids and save to table (E)
+    unflat_table = DM.CopyRows(self_union, 'unflat_table')
+    DM.AddField(unflat_table, zone_id, zone_type) # default text length of 50 is fine if needed
+    with arcpy.da.UpdateCursor(unflat_table, [fid1, zone_id]) as u_cursor:
+        for row in u_cursor:
+            row[1] = zoneid_dict[row[0]] # assign zone id
+            u_cursor.updateRow(row)
+
+    # Delete Identical (C) (save as flat[zone])
+    DM.CopyFeatures(self_union, 'flatzone')
+    DM.DeleteIdentical('flatzone', flat_zoneid)
+    DM.Delete(self_union) # large and we're done with it
+
+    # Run Stats tool on C (D)
+    flatzone_stats_table = stats_area_table('flatzone', flat_zoneid, in_value_raster, 'temp_out_table', is_thematic)
+    count_diff = flatzone_stats_table[1]
+    flatzone_stats_table = flatzone_stats_table[0]
+
+
+    # map original zone ids to new zone ids
+    original_flat = defaultdict(list)
+    with arcpy.da.SearchCursor(unflat_table, [zone_id, flat_zoneid]) as cursor:
+        for row in cursor:
+            if row[1] not in original_flat[row[0]]:
+                original_flat[row[0]].append(row[1])
+
+    # Use CELL_COUNT as weight for means to calculate final values for each zone.
+    i_cursor = arcpy.da.InsertCursor(result, [zone_id, 'CELL_COUNT', 'MEAN', 'DataCoverage_pct']) # open output table cursor
+    flat_stats = {r[0]:r[1:] for r in arcpy.da.SearchCursor(flatzone_stats_table, [flat_zoneid, 'CELL_COUNT', 'MEAN', 'DataCoverage_pct'])}
+
+    for zid, unflat_ids in original_flat.items():
+        area_vec = [float(flat_stats[id][0]) for id in unflat_ids] # CELL_COUNT
+        mean_vec = [flat_stats[id][1] for id in unflat_ids] # MEAN
+        pct_vec = [flat_stats[id][2] for id in unflat_ids] # DataCoverage_pct
+        weighted_mean = sum([a*b for a,b in zip(area_vec, mean_vec)])/sum(area_vec)
+        weighted_pct = sum([a*b for a,b in zip(area_vec, pct_vec)])/sum(area_vec)
+        cell_count = sum(area_vec)
+        i_cursor.insertRow((zid, cell_count, weighted_mean, weighted_pct))
+
+    del i_cursor
+
+    for item in [unflat_table, identical_shapes, 'flatzone', flatzone_stats_table]:
+        DM.Delete(item)
+
+    return [result, count_diff]
+
+def handle_overlaps(zone_fc, zone_field, zone_has_overlaps, in_value_raster, out_table, is_thematic, debug_mode = False):
+    if zone_has_overlaps:
+        out_table = flatten_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic)
+    else:
+        out_table = stats_area_table(zone_fc, zone_field, in_value_raster, out_table, is_thematic, debug_mode)
+
+    total_count_diff = out_table[1]
 
     if total_count_diff > 0:
-        warn_msg = ("WARNING: {0} zones have null zonal statistics. There are 3 possible reasons:\n"
+        warn_msg = ("WARNING: {0} zones have null zonal statistics. There are 2 possible reasons:\n"
                     "1) Presence of zones that are fully outside the extent of the raster summarized.\n"
-                    "2) Overlapping zones in the input. Use Subset Overlapping Zones tool, then run again.\n"
-                    "3) Zones are too small relative to the raster resolution.".format(total_count_diff))
+                    "2) Zones are too small relative to the raster resolution.".format(total_count_diff))
         arcpy.AddWarning(warn_msg)
+    return out_table
 
 def main():
     zone_fc = arcpy.GetParameterAsText(0)
     zone_field = arcpy.GetParameterAsText(1)
-    in_value_raster = arcpy.GetParameterAsText(2)
-    out_table = arcpy.GetParameterAsText(4)
-    is_thematic = arcpy.GetParameter(3) #boolean
-    handle_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic)
-
-
-# def test(out_table, is_thematic = False):
-#     arcpy.env.overwriteOutput = True
-#     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-#     test_data_gdb = os.path.abspath(os.path.join(os.pardir, 'TestData_0411.gdb'))
-#     zone_fc = os.path.join(test_data_gdb, 'HU12')
-#     zone_field = 'ZoneID'
-#     if is_thematic:
-#         in_value_raster = os.path.join(test_data_gdb, 'NLCD_LandCover_2006')
-#     else:
-#         in_value_raster = os.path.join(test_data_gdb, 'Total_Nitrogen_Deposition_2006')
-#     handle_overlaps(zone_fc, zone_field, in_value_raster, out_table, is_thematic, debug_mode = True)
-#     arcpy.env.overwriteOutput = False
+    zone_has_overlaps = arcpy.GetParameter(2) # boolean
+    in_value_raster = arcpy.GetParameterAsText(3)
+    out_table = arcpy.GetParameterAsText(5)
+    is_thematic = arcpy.GetParameter(4) # boolean
+    handle_overlaps(zone_fc, zone_field, zone_has_overlaps, in_value_raster, out_table, is_thematic)
 
 if __name__ == '__main__':
     main()

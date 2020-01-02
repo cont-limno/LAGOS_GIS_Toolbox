b0bf96495a9de6e3f67dd506bc2732d7a8084070
Nicole Jean Smith <nicole.j.smith@gmail.com>
2020-01-02 14:05:04 -0500

    simplify to repair geometry for all and prevent 999999 failures on FindIdentical by enforcing non-existence of output

--- lagosGIS/flatten_overlaps.py
+++ lagosGIS/flatten_overlaps.py
@@ -4,7 +4,7 @@ from arcpy import management as DM
 from arcpy import analysis as AN
 
 
-def flatten_overlaps(zone_fc, zone_field, output_fc, output_table):
+def flatten_overlaps(zone_fc, zone_field, output_fc, output_table, cluster_tolerance=' 3 Meters'):
     orig_env = arcpy.env.workspace
     arcpy.env.workspace = 'in_memory'
 
@@ -17,19 +17,21 @@ def flatten_overlaps(zone_fc, zone_field, output_fc, output_table):
     # Union with FID_Only (A)
     arcpy.AddMessage("Splitting overlaps in polygons...")
     zoneid_dict = {r[0]: r[1] for r in arcpy.da.SearchCursor(zone_fc, [objectid, zone_field])}
-    self_union = AN.Union([zone_fc], 'self_union', 'ONLY_FID', cluster_tolerance='1 Meters')
+    self_union = AN.Union([zone_fc], 'self_union', 'ONLY_FID', cluster_tolerance=cluster_tolerance)
 
     # #If you don't run this section, Find Identical fails with error 999999. Seems to have to do with small slivers
     # #having 3 vertices and/or only circular arcs in the geometry.
     arcpy.AddMessage("Repairing self-union geometries...")
-    DM.AddGeometryAttributes(self_union, 'POINT_COUNT; AREA')
-    union_fix = DM.MakeFeatureLayer(self_union, 'union_fix', where_clause='PNT_COUNT <= 4 OR POLY_AREA < 0.01')
-    arcpy.Densify_edit(union_fix, 'OFFSET', max_deviation='1 Meters')  # selection ON, edits self_union disk
-    DM.RepairGeometry(union_fix, 'DELETE_NULL')  # eliminate empty geoms. selection ON, edits self_union disk
-    for field in ['PNT_COUNT', 'POLY_AREA']:
-        DM.DeleteField(self_union, field)
+    # DM.AddGeometryAttributes(self_union, 'POINT_COUNT; AREA')
+    # union_fix = DM.MakeFeatureLayer(self_union, 'union_fix', where_clause='PNT_COUNT <= 10 OR POLY_AREA < 5000')
+    # arcpy.Densify_edit(union_fix, 'DISTANCE', distance = '1 Meters', max_deviation='1 Meters')  # selection ON, edits self_union disk
+    DM.RepairGeometry(self_union, 'DELETE_NULL')  # eliminate empty geoms. selection ON, edits self_union disk
+    # for field in ['PNT_COUNT', 'POLY_AREA']:
+    #     DM.DeleteField(self_union, field)
 
     # Find Identical by Shape (B)
+    if arcpy.Exists('identical_shapes'):
+        DM.Delete('identical_shapes') # causes failure in FindIdentical even when overwrite is allowed
     identical_shapes = DM.FindIdentical(self_union, 'identical_shapes', 'Shape')
 
     # Join A to B and calc flat[zone]_zoneid = FEAT_SEQ (C)
@@ -66,20 +68,20 @@ def flatten_overlaps(zone_fc, zone_field, output_fc, output_table):
     output_fc = DM.CopyFeatures(self_union, output_fc)
     output_table = DM.CopyRows(unflat_table, output_table)
 
-    return output_fc
-
     # cleanup
-    for item in [self_union, identical_shapes, unflat_table, union_fix]:
+    for item in [self_union, identical_shapes, unflat_table]:
         DM.Delete(item)
     arcpy.env.workspace = orig_env
+
+    return output_fc
 
 def main():
     zone_fc = arcpy.GetParameterAsText(0)
     zone_field = arcpy.GetParameterAsText(1)
     output_fc = arcpy.GetParameterAsText(2)
     output_table = arcpy.GetParameterAsText(3)
-
-    flatten_overlaps(zone_fc, zone_field, output_fc, output_table)
+    cluster_tolerance = arcpy.GetParameterAsText(4)
+    flatten_overlaps(zone_fc, zone_field, output_fc, output_table, cluster_tolerance)
 
 if __name__ == '__main__':
     main()

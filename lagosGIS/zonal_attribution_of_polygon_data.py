2f458a46c1e227fe00fbbb17a7ca9b17a4c1e422
Nicole Jean Smith <nicole.j.smith@gmail.com>
2020-01-06 15:30:06 -0500

    add renaming to standard option

--- lagosGIS/zonal_attribution_of_polygon_data.py
+++ lagosGIS/zonal_attribution_of_polygon_data.py
@@ -1,19 +1,44 @@
+import csv
 import os
 import arcpy
+import csiutils as cu
 
-def zonal_attribution_of_polygon_data(zone_fc, zone_field, class_fc, output_table, class_field, class_name):
+
+def zonal_attribution_of_polygon_data(zone_fc, zone_field, class_fc, out_table, class_field, rename_tag=''):
+
+    def rename_to_standard(table):
+        arcpy.AddMessage("Renaming.")
+
+        # look up the values based on the rename tag
+        this_files_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(this_files_dir)
+        geo_file = os.path.abspath('../geo_metric_provenance.csv')
+        with open(geo_file) as csv_file:
+            reader = csv.DictReader(csv_file)
+            mapping = {row['subgroup_original_code']: row['subgroup']
+                       for row in reader if row['main_feature'] in rename_tag and row['main_feature']}
+            arcpy.AddMessage(mapping)
+
+        # update them
+        for old, new in mapping.items():
+            arcpy.AddMessage(new)
+            old_fname = '{}'.format(old)
+            new_fname = '{}_{}_pct'.format(rename_tag, new)
+            if arcpy.ListFields(table, old_fname):
+                try:
+                    # same problem with AlterField limit of 31 characters here.
+                    arcpy.AlterField_management(table, old_fname, new_fname, clear_field_alias=True)
+                except:
+                    cu.rename_field(table, old_fname, new_fname, deleteOld=True)
+        return table
+
+
     arcpy.env.workspace = 'in_memory'
     tab = arcpy.TabulateIntersection_analysis(zone_fc, zone_field, class_fc, 'tab', class_field)
     pivot = arcpy.PivotTable_management(tab, zone_field, class_field, "PERCENTAGE", 'pivot')
-    fnames = [f.name for f in arcpy.ListFields(pivot) if f.type not in ('OID', 'Geometry')]
-    fnames.remove(zone_field)
-    print(fnames)
-    for f in fnames:
-        zone_prefix = os.path.basename(zone_fc)
-        new_fname = '{}_{}_{}_pct'.format(zone_prefix, class_name, f).lower()
-        arcpy.AlterField_management(pivot, f, new_fname, clear_field_alias=True)
-    arcpy.CopyRows_management(pivot, output_table)
-    for item in [tab, pivot]:
+    renamed = rename_to_standard(pivot)
+    arcpy.CopyRows_management(renamed, out_table)
+    for item in [tab, pivot, renamed]:
         arcpy.Delete_management(item)
 
 def main():
@@ -22,8 +47,8 @@ def main():
     class_fc = arcpy.GetParameterAsText(2)
     output_table = arcpy.GetParameterAsText(3)
     class_field = arcpy.GetParameterAsText(4)
-    class_name = arcpy.GetParameterAsText(5)
-    zonal_attribution_of_polygon_data(zone_fc, zone_field, class_fc, output_table, class_field, class_name)
+    rename_tag = arcpy.GetParameterAsText(5)
+    zonal_attribution_of_polygon_data(zone_fc, zone_field, class_fc, output_table, class_field, rename_tag)
 
 if __name__ == '__main__':
     main()

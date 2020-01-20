0a050e73f8c2d4abe433e9b9f803dc0411d3eb9b
Nicole Jean Smith <nicole.j.smith@gmail.com>
2020-01-20 13:27:54 -0500

    add densify and delete small features after union to permit run

--- lagosGIS/preprocess_padus.py
+++ lagosGIS/preprocess_padus.py
@@ -1,25 +1,40 @@
+import os
+import time
 import arcpy
 from arcpy import management as DM
 from lagosGIS import select_fields
 
+
 def preprocess(padus_combined_fc, output_fc):
     arcpy.env.workspace = 'in_memory'
-    padus_fields = ['FeatClass', 'Own_Type', 'GAP_Sts', 'IUCN_Cat']
-    padus_select = select_fields(padus_combined_fc, 'padus_select', padus_fields)
-    union = arcpy.Union_analysis([padus_select, padus_select], 'union', 'ALL')
 
+    # Prep: Select only the fields needed, remove curves (densify) which prevents problems with geometry
+    # that prevents DeleteIdentical based on Shape
+    padus_fields = ['FeatClass', 'Own_Type', 'GAP_Sts', 'IUCN_Cat']
+    arcpy.AddMessage('{} select...'.format(time.ctime()))
+    padus_select = select_fields(padus_combined_fc, 'padus_select', padus_fields, convert_to_table=False)
+    arcpy.Densify_edit(padus_select, 'OFFSET', max_deviation = '1 Meters')
+    arcpy.AddMessage('{} union...'.format(time.ctime()))
+
+    # self-union
+    union = arcpy.Union_analysis([padus_select, padus_select], 'union', 'ALL', cluster_tolerance='1 Meters')
+
+    # self-union creates duplicates, remove
     fid1 = 'FID_padus_select'
     fid2 = 'FID_padus_select_1'
     padus_fields_1 = padus_fields + [f + '_1' for f in padus_fields]
     padus_fields_1.extend([fid1, fid2])
+    arcpy.AddMessage('{} delete identical round 1...'.format(time.ctime()))
     DM.DeleteIdentical(union, padus_fields_1)
 
-    new_fields = ['agency', 'gap', 'iucn', 'merge_flag']
-    cursor_fields = sorted(padus_fields_1) + new_fields
+    # calculate the new class values based on the overlaid polygons
+    new_fields = ['agency', 'gap', 'iucn', 'merge_flag', 'area_m2']
+    cursor_fields = sorted(padus_fields_1) + new_fields + ['SHAPE@AREA']
     DM.AddField(union, 'agency', 'TEXT', field_length=5)
     DM.AddField(union, 'gap', 'TEXT', field_length=1)
     DM.AddField(union, 'iucn', 'TEXT', field_length=24)
     DM.AddField(union, 'merge_flag', 'TEXT', field_length=1)
+    DM.AddField(union, 'area_m2', 'DOUBLE')
 
     owner_rule = {'Fee': 1, 'Easement': 2, 'Marine': 3, 'Designation': 4}
     iucn_rule = {'Ia': 1,
@@ -32,9 +47,10 @@ def preprocess(padus_combined_fc, output_fc):
                  'Other Conservation Area': 8,
                  'Unassigned': 9}
 
+    arcpy.AddMessage('{}  calculate...'.format(time.ctime()))
     with arcpy.da.UpdateCursor(union, cursor_fields) as cursor:
         for row in cursor:
-            id1, id2, fc1, fc2, gap1, gap2, iucn1, iucn2, own1, own2, agency, gap, iucn, flag = row
+            id1, id2, fc1, fc2, gap1, gap2, iucn1, iucn2, own1, own2, agency, gap, iucn, flag, areacalc, areashp = row
             flag = 'N'
             # take fee first, designation last to pull owner type from
             if owner_rule[fc1] < owner_rule[fc2]:
@@ -54,15 +70,30 @@ def preprocess(padus_combined_fc, output_fc):
             if fc1 <> fc2 or own1 <> own2 or gap1 <> gap2 or iucn1 <> iucn2:
                 flag = 'Y'
 
-            row = (id1, id2, fc1, fc2, gap1, gap2, iucn1, iucn2, own1, own2, agency, gap, iucn, flag)
+            areacalc = areashp
+
+            row = (id1, id2, fc1, fc2, gap1, gap2, iucn1, iucn2, own1, own2, agency, gap, iucn, flag, areacalc, areashp)
             cursor.updateRow(row)
 
-    sorted_fc = DM.Sort(union, 'sorted_fc', [['merge_flag', 'DESCENDING']])
+
+    # Prep for DeleteIdentical: Dispose of small polygons (they cause trouble, don't effect
+    # stats enough to bother) and repair geometry on the rest.
+    large_enough = arcpy.Select_analysis(union, 'large_enough', 'area_m2 > 4')
+    arcpy.AddMessage('{} repair...'.format(time.ctime()))
+    DM.RepairGeometry(large_enough)
+
+    # Sort so that merged polygons are retained in DeleteIdentical, and delete identical shapes to end
+    # up with just the merged polygons and non-overlapping polygons
+    arcpy.AddMessage('{}  sort...'.format(time.ctime()))
+    sorted_fc = DM.Sort(large_enough, 'sorted_fc', [['merge_flag', 'DESCENDING']])
+
+    arcpy.AddMessage('{}  delete identical shape...'.format(time.ctime()))
     DM.DeleteIdentical(sorted_fc, "Shape")
     output_fields = [fid1, fid2] + new_fields
     output_fc = select_fields(sorted_fc, output_fc, output_fields)
 
-    for item in [padus_select, union, sorted_fc]:
+    # cleanup
+    for item in [padus_select, union, sorted_fc, large_enough]:
         DM.Delete(item)
     return output_fc
 

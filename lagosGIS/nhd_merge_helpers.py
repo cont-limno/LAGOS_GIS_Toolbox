a1d31c2bad2170d3785f42f9ad15ce38e6cc5e74
Nicole Jean Smith <nicole.j.smith@gmail.com>
2020-10-08 11:40:34 -0400

    move and rename

--- lagosGIS/nhd_merge_helpers.py
+++ lagosGIS/nhd_merge_helpers.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import os, time
 import arcpy
-"""
-This module contains custom functions necessary to reproduce the research the Lakes in the US paper.
-"""
+
 
 def batch_add_merge_ids(nhd_parent_directory):
     """
@@ -74,49 +72,6 @@ def batch_add_merge_ids2(nhd_parent_directory):
             for row in cursor:
                 row[2] = '{}_{}'.format(row[0], str(row[1]))
                 cursor.updateRow(row)
-        #arcpy.CalculateField_management(fc, "nhd_merge_id", '''!Permanent_Identifier! + '_' + str(!FDate!)''', "PYTHON")
-
-def efficient_merge(feature_class_list, output_fc, filter =''):
-    fc_count = len(feature_class_list)
-    all_exist_test = all(arcpy.Exists(fc) for fc in feature_class_list)
-
-    # EXECUTE
-    # Start with FC containing largest extent to prevent spatial grid errors
-    descriptions = [arcpy.Describe(fc).extent for fc in feature_class_list]
-    fc_areas = [int(d.XMax-d.XMin) * int(d.YMax-d.YMin) for d in descriptions]
-    index = [i for i, x in enumerate(fc_areas) if x == max(fc_areas)]
-    first_fc = feature_class_list[index[0]]
-    indexes = arcpy.ListIndexes(first_fc)
-    feature_class_list.remove(first_fc)
-
-    # This is a fast and stable merge method for this number of features compared to arcpy Merge
-    if all_exist_test:
-        print("Beginning merge of {} feature classes, copying first feature class to output...".format(fc_count))
-        arcpy.Select_analysis(first_fc, output_fc, filter)
-        arcpy.SetLogHistory = False  # speeds up iterative updates, won't write to geoprocessing for every step
-        insertRows = arcpy.da.InsertCursor(output_fc, ["SHAPE@", "*"])
-
-        for fc in feature_class_list:
-            searchRows = arcpy.da.SearchCursor(fc, ["SHAPE@", "*"], filter)
-            counter = 0
-            for searchRow in searchRows:
-                insertRows.insertRow(searchRow)
-                counter +=1
-            del searchRow, searchRows
-            print("Merged {0} features from {1}".format(counter, fc))
-        del insertRows
-        arcpy.SetLogHistory = True
-
-        # Rebuild indexes
-        try:
-            arcpy.AddIndex_management(output_fc, 'Permanent_Identifier', 'IDX_Permanent_Identifier')
-        except:
-           arcpy.AddWarning('Could not build Permanent_Identifier index because there is no such field.')
-
-    else:
-        print("ERROR: One or more feature class paths is not valid. Merged feature class not created.")
-
-
 
 
 def deduplicate_nhd(in_feature_class_or_table, out_feature_class_or_table ='', unique_id ='Permanent_Identifier'):

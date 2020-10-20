e7d362d6c7b88910ebeb7a12765238230c5ec61c
Nicole Jean Smith <nicole.j.smith@gmail.com>
2020-10-20 08:37:30 -0400

    add toolbox tool: Add Lake Seeds

---
+++ lagosGIS/watershed_delineation/add_lake_seeds_toolbox.py
@@ -0,0 +1,10 @@
+import arcpy
+import nhd_plus_watersheds_tools as npwt
+
+nhdplus_catseed_raster = arcpy.GetParameterAsText(0)
+nhdplus_gdb = arcpy.GetParameterAsText(1)
+gridcode_table = arcpy.GetParameterAsText(2)
+eligible_lakes_fc = arcpy.GetParameterAsText(3)
+output_raster = arcpy.GetParameterAsText(4)
+
+npwt.add_lake_seeds(nhdplus_catseed_raster, nhdplus_gdb, gridcode_table, eligible_lakes_fc, output_raster)

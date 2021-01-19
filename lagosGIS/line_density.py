ba8a3bf443a34545c6e64b8e0f6de9361bd391ca
Nicole Jean Smith <nicole.j.smith@gmail.com>
2021-01-19 10:47:58 -0500

    add messages around long-running identity step

--- lagosGIS/line_density.py
+++ lagosGIS/line_density.py
@@ -18,7 +18,9 @@ def calc_density(zones_fc, zone_field, lines_fc, out_table, where_clause='', ren
     # TODO: Add projection check
 
     # Perform identity analysis to join fields and crack lines at polygon boundaries
+    arcpy.AddMessage("Cracking lines...")
     lines_identity = arcpy.Identity_analysis(lines_prep, zones_fc, 'lines_identity')
+    arcpy.AddMessage("Summarizing results...")
     arcpy.AddField_management(lines_identity, 'length_m', 'DOUBLE')
     arcpy.CalculateField_management(lines_identity, 'length_m', '!shape.length!', 'PYTHON')
 

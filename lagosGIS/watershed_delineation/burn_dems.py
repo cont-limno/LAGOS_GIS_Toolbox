eab13b8c2399a1a041e7d1670d979996fe98120d
Nicole Jean Smith <nicole.j.smith@gmail.com>
2021-02-16 09:44:31 -0500

    Modify burn_dems to 1) accept NHDPlus BurnLineEvent as burn input and 2) protect all LAGOS isolated/closed lakes as sinks

--- lagosGIS/watershed_delineation/burn_dems.py
+++ lagosGIS/watershed_delineation/burn_dems.py
@@ -8,13 +8,14 @@ import os
 import arcpy
 from arcpy.sa import *
 from arcpy import env
+from NHDNetwork import NHDNetwork
 
 
 def burn_streams(subregion_ned, nhd_gdb, burnt_out,
                 projection = arcpy.SpatialReference(102039)):
     """
     Enforce stream linework in hydrologic conditioning of DEM and add walls at the HU12 boundaries.
-    :param subregion_ned: A mosaicked (NED) DEM covering the subregion being processed
+    :param subregion_ned: A mosaicked (NED) DEM covering the subregion being processed with elevation in cm
     :param nhd_gdb: The NHD HR subregion geodatabase being processed
     :param burnt_out: Path of the output raster with burned streams
     :param projection: Optional, default is USGS Albers (102039)
@@ -27,7 +28,10 @@ def burn_streams(subregion_ned, nhd_gdb, burnt_out,
     env.extent = subregion_ned
     env.workspace = 'in_memory'
 
-    flowline = os.path.join(nhd_gdb, 'NHDFlowline')
+    burnline = os.path.join(nhd_gdb, 'NHDBurnLineEvent')
+    if not arcpy.Exists(burnline):
+        burnline = os.path.join(nhd_gdb, 'NHDFlowline')
+
     nhdarea = os.path.join(nhd_gdb, 'NHDArea')
     nhdwaterbody = os.path.join(nhd_gdb, 'NHDWaterbody')
 
@@ -35,11 +39,25 @@ def burn_streams(subregion_ned, nhd_gdb, burnt_out,
     # just easier to have a copy in the correct projection later
 
     # 428 = pipeline, 336 = canal, flow direction must be initialized--matches NHDPlus rules pretty well
+    # will have no effect if using burnline layer
     flowline_eligible_query = 'FType NOT IN (428,336) OR (FType = 336 and FlowDir = 1)'
-    flowline_burn = arcpy.Select_analysis(flowline, 'flowline_proj', flowline_eligible_query)
+    flowline_burn = arcpy.Select_analysis(burnline, 'flowline_proj', flowline_eligible_query)
     area_burn = arcpy.Select_analysis(nhdarea, 'area_burn', 'FType = 460') # StreamRiver only
     waterbody_burn = arcpy.Select_analysis(nhdwaterbody, 'waterbody_burn', 'FType IN (390, 436)')
 
+    # identify valid sinks
+    network = NHDNetwork(nhd_gdb)
+    waterbody_ids = network.define_lakes(strict_minsize=False, force_lagos=True).keys()
+    arcpy.AddMessage("Identifying sink lakes...")
+    lake_conn_classes = {id:network.classify_waterbody_connectivity(id) for id in waterbody_ids}
+    sink_lake_ids = [k for k,v in lake_conn_classes.items() if v in ('Isolated', 'TerminalLk', 'Terminal')]
+    sink_lakes_query = 'Permanent_Identifier IN ({})'.format(
+        ','.join(['\'{}\''.format(id) for id in sink_lake_ids]))
+    sink_lakes = arcpy.Select_analysis(network.waterbody, 'sink_lakes', sink_lakes_query)
+
+    # protect these sink lakes in DEM
+    sink_centroids = arcpy.FeatureToPoint_management(sink_lakes, 'sink_centroids', 'INSIDE')
+    sink_centroids_raster = arcpy.PointToRaster_conversion(sink_centroids, "OBJECTID", "centroids_raster", cellsize=10)
 
     # Feature to Raster- rasterize the feature classes
     # will inherit grid from env.snapRaster
@@ -61,11 +79,11 @@ def burn_streams(subregion_ned, nhd_gdb, burnt_out,
     arcpy.MakeFeatureLayer_management(huc12_fc, "huc12_layer")
 
     # make the walls raster object
-    arcpy.PolygonToLine_management(huc12_fc, 'wall_lines')
-    arcpy.AddField_management('wall_lines', "height", "FLOAT")
+    wall_lines = arcpy.PolygonToLine_management(huc12_fc, 'wall_lines')
+    arcpy.AddField_management(wall_lines, "height", "FLOAT")
     wall_ht = 500000
-    arcpy.CalculateField_management('wall_lines', "height", '{}'.format(wall_ht), "PYTHON")
-    arcpy.FeatureToRaster_conversion('wall_lines', "height", 'wall_raster')
+    arcpy.CalculateField_management(wall_lines, "height", '{}'.format(wall_ht), "PYTHON")
+    arcpy.FeatureToRaster_conversion(wall_lines, "height", 'wall_raster')
     walls = Raster('wall_raster')
 
     # convert heights to cm and round to 1 mm, to match NHDPlus
@@ -73,27 +91,33 @@ def burn_streams(subregion_ned, nhd_gdb, burnt_out,
     streams = Reclassify(Raster(flowline_raster) > 0, "Value", "1 1; NoData 0")
     banks = Reclassify(Raster(nhdarea_raster) > 0, "Value", "1 1; NoData 0")
     lakes = Reclassify(Raster(lakes_raster) > 0, "Value", "1 1; NoData 0")
+    sinks = Reclassify(Raster(sink_centroids_raster) > 0, "Value", "1 1; NoData 0")
 
     width = 60 # in horizontal map units
     soft_drop = 5000 # cm
     sharp_drop = 100000 # cm
     areal_drop = 10000 # cm
-    burnt = 100 * Raster(subregion_ned) \
+    burnt = Raster(subregion_ned) \
             - (sharp_drop * streams) \
             - (areal_drop * banks) \
             - (areal_drop * lakes) \
             + soft_drop * ((distance/width) - 1) * (distance < width) # beveled drop, convert distance to cm
+    burnt_with_sinks = SetNull(sinks, burnt)
     no_wall = BooleanOr(IsNull(walls), streams) # allow streams to cut walls
-    walled = Con(no_wall, burnt, (burnt + walls))
+    walled = Con(no_wall, burnt_with_sinks, (burnt_with_sinks + walls))
 
     arcpy.AddMessage("Saving output raster...")
-    walled.save(burnt_out)
+    walled.save('in_memory/walled_save')
+    arcpy.CopyRaster_management('in_memory/walled_save', burnt_out) # 2 steps in order to use raster compression
+
 
     # Delete intermediate rasters and shapefiles
-    for item in [flowline_burn, area_burn, waterbody_burn, flowline_raster, nhdarea_raster, lakes_raster, 'huc12_layer']:
-        arcpy.Delete_management(item)
+    for item in [flowline_burn, area_burn, waterbody_burn,
+                 sink_lakes, sink_centroids, sink_centroids_raster,
+                 flowline_raster, nhdarea_raster, lakes_raster, 'huc12_layer',
+                 wall_lines, 'walled_save']:
+      arcpy.Delete_management(item)
     arcpy.CheckInExtension("Spatial")
-    arcpy.AddMessage("Burn process completed")
     arcpy.ResetEnvironments()
     return burnt_out
 

d8c843b8a6cad6ed5c4118550795cbd509ece22f
Nicole Jean Smith <nicole.j.smith@gmail.com>
2021-05-25 15:05:32 -0400

    add header

--- lagosGIS/watershed_delineation/make_hydrodem_toolbox.py
+++ lagosGIS/watershed_delineation/make_hydrodem_toolbox.py
@@ -1,3 +1,9 @@
+# filename: make_hydrodem_toolbox.py
+# author: Nicole J Smith
+# version: 2.0 Beta
+# LAGOS module(s): LOCUS
+# tool type: re-usable (ArcGIS Toolbox)
+
 import arcpy
 import nhd_plus_watersheds_tools as npwt
 

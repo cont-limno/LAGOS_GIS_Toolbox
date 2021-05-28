a68e9786eb89dfcf72e749c727ed46674922d4c7
Nicole Jean Smith <nicole.j.smith@gmail.com>
2021-05-28 14:03:58 -0400

    update input parameters to include nhd_fdr

--- lagosGIS/watershed_delineation/flow_direction_toolbox.py
+++ lagosGIS/watershed_delineation/flow_direction_toolbox.py
@@ -8,6 +8,7 @@ import arcpy
 import nhd_plus_watersheds_tools as npwt
 
 hydrodem_raster = arcpy.GetParameterAsText(0)
-flow_direction_raster_out = arcpy.GetParameterAsText(1)
+nhd_fdr = arcpy.GetParameterAsText(1)
+flow_direction_raster_out = arcpy.GetParameterAsText(2)
 
-npwt.flow_direction(hydrodem_raster, flow_direction_raster_out)
+npwt.flow_direction(hydrodem_raster, nhd_fdr, flow_direction_raster_out)

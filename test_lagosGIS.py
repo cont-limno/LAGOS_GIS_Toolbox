164527929ebb0676233efe98299bda7f4bfb5cc3
Nicole Jean Smith <nicole.j.smith@gmail.com>
2022-01-31 15:52:24 -0500

    update names

--- test_lagosGIS.py
+++ test_lagosGIS.py
@@ -10,17 +10,17 @@ TEST_DATA_GDB = os.path.abspath(os.path.join(os.curdir, 'TestData_0411.gdb'))
 arcpy.env.overwriteOutput = True
 
 __all__ = ["lake_connectivity_classification",
-           "zonal_attribution_of_raster_data",
-           "polygons_in_zones",
-           "lakes_in_zones",
-           "interlake_watersheds_US",
+           "zonal_summary_of_raster_data",
+           "polygon_density_in_zones",
+           "lake_density",
+           "aggregate_watersheds",
            "upstream_lakes",
            "point_attribution_of_raster_data"]
 
 def lake_connectivity_classification(out_feature_class, debug_mode = True):
     lagosGIS.lake_connectivity_classification(TEST_DATA_GDB, out_feature_class, debug_mode)
 
-def zonal_attribution_of_raster_data(out_table, overlaps=False, is_thematic=False):
+def zonal_summary_of_raster_data(out_table, overlaps=False, is_thematic=False):
     if overlaps:
         zone_fc = os.path.join(TEST_DATA_GDB, 'flatbuff500_raster')
         zone_field = 'flatbuff500_zoneid'
@@ -37,7 +37,7 @@ def zonal_attribution_of_raster_data(out_table, overlaps=False, is_thematic=Fals
         in_value_raster = os.path.join(TEST_DATA_GDB, 'Total_Nitrogen_Deposition_2006')
         rename_tag = 'wetdepno3_2006'
         units = 'kgperha'
-    lagosGIS.zonal_attribution_of_raster_data(zone_fc, zone_field, in_value_raster, out_table, is_thematic=is_thematic,
+    lagosGIS.zonal_summary_of_raster_data(zone_fc, zone_field, in_value_raster, out_table, is_thematic=is_thematic,
                                               unflat_table=unflat_table, rename_tag=rename_tag, units=units)
 # hand-verified to be correct
 # update 2018-05-29, these numbers rely on the old method of using the source raster data grid instead of the
@@ -46,27 +46,21 @@ hu12_7509_totalN_2006 = {"CELL_COUNT": 7, "MIN": 6.749966, "MAX": 7.41714, "MEAN
 hu12_7454_totalN_2006 = {"CELL_COUNT": None, "MIN": None, "MAX": None, "MEAN": None, "STD": None}
 
 
-def polygons_in_zones(out_table, selection_expression = ''):
+def polygon_density_in_zones(out_table, selection_expression = ''):
     zone_fc = os.path.join(TEST_DATA_GDB, 'HU12')
     polygons_fc = os.path.join(TEST_DATA_GDB, 'Lakes_1ha')
-    lagosGIS.polygons_in_zones(zone_fc, 'ZoneID', polygons_fc, out_table, selection_expression)
+    lagosGIS.polygon_density_in_zones(zone_fc, 'ZoneID', polygons_fc, out_table, selection_expression)
 
-def lakes_in_zones(out_table):
+def lake_density(out_table):
     zone_fc = os.path.join(TEST_DATA_GDB, 'HU12')
     polygons_fc = os.path.join(TEST_DATA_GDB, 'Lakes_1ha')
-    lagosGIS.lakes_in_zones(zone_fc, 'ZoneID', polygons_fc, out_table)
+    lagosGIS.lake_density(zone_fc, 'ZoneID', polygons_fc, out_table)
 
-def interlake_watersheds_NE(out_fc):
-    watersheds = os.path.join(TEST_DATA_GDB, 'Local_Catchments_Original_Methods')
-    nhd_gdb = TEST_DATA_GDB
-    eligible_lakes = os.path.join(TEST_DATA_GDB, 'eligible_lakes')
-    lagosGIS.aggregate_watersheds_NE(watersheds, nhd_gdb, eligible_lakes, out_fc, mode = 'interlake')
-
-def interlake_watersheds_US(out_fc):
+def aggregate_watersheds(out_fc):
     # watersheds = r'C:\Users\smithn78\Dropbox\CL_HUB_GEO\QAQC\New_Watershed_Methods\Test.gdb\lake_and_flowline_catchments_feb27'
     nhdplus_gdb = r'D:\Not_ContLimno\NHDPlus HR\NHDPlus_H_0205_GDB.gdb'
     eligible_lakes = r'C:\Users\smithn78\Dropbox\CL_HUB_GEO\LAGOS_US_GIS_Data_v0.5.gdb\Lakes\LAGOS_US_All_Lakes_1ha'
-    lagosGIS.aggregate_watersheds_US(nhdplus_gdb, eligible_lakes, out_fc, mode = 'interlake')
+    lagosGIS.aggregate_watersheds(nhdplus_gdb, eligible_lakes, out_fc, mode = 'interlake')
 
 def upstream_lakes(out_table):
     lagosGIS.upstream_lakes(TEST_DATA_GDB, out_table)
@@ -74,5 +68,5 @@ def upstream_lakes(out_table):
 def point_attribution_of_raster_data(out_table):
     zone_points = os.path.join(TEST_DATA_GDB, 'Lakes_1ha_Point')
     in_value_raster = os.path.join(TEST_DATA_GDB, 'Total_Nitrogen_Deposition_2006')
-    lagosGIS.point_attribution_of_raster_data(zone_points, 'Permanent_Identifier', in_value_raster, out_table,
+    lagosGIS.attribution(zone_points, 'Permanent_Identifier', in_value_raster, out_table,
                                               'lake_wetdepinorgnitrogen', 'kgperha')

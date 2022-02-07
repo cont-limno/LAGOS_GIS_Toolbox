9dbc290c0564563327cfdc526009a9e01fc1642e
Nicole Jean Smith <nicole.j.smith@gmail.com>
2022-02-07 09:42:36 -0500

    move job control csv into toolbox

--- lagosGIS/run_tools/batch_run_job_control.py
+++ lagosGIS/run_tools/batch_run_job_control.py
@@ -8,6 +8,6 @@ import read_job_control as rjc
 
 va = ['Arg1', 'Arg3', 'Arg6']
 pva = ['Arg1', 'Arg3']
-job_csv = r"C:\Users\smithn78\Dropbox\CL_HUB_GEO\Metrics_Job_Control\raster_job_control.csv"
+job_csv = '../geo_job_control.csv'
 
 rjc.read_job_control(job_csv)

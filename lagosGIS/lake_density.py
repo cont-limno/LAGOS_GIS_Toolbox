57d20ef5ffc6cb6c4599e5376a2a634e2a1cbab1
Nicole Jean Smith <nicole.j.smith@gmail.com>
2022-02-15 17:35:46 -0500

    add docstring, comments

--- lagosGIS/lake_density.py
+++ lagosGIS/lake_density.py
@@ -9,14 +9,25 @@ import arcpy
 import polygon_density_in_zones
 import lagosGIS
 
-# Since the ws & nws layers were left with the jagged "rasterization" lines, they sometimes overlap lakes outside
-# their area with small slivers. This function pre-processes the watersheds layer to erase those small sliver overlaps
-# so that the lake count is more accurate.
-# This tool is pretty slow.
 
 def trim_watershed_slivers(watersheds_fc, lakes_fc, output_fc):
+    """
+    Align watershed and lake layers by trimming overlapping slivers from watersheds layer in order to refine the
+    lake count. These overlaps are present because the geometry for these features follows the 10m raster cells used
+    to delinate the watersheds, whereas the lakes do not.
+
+    This tool is pretty slow.
+    :param watersheds_fc: The ws/nws polygon feature class
+    :param lakes_fc: The lakes feature class to be counted
+    :param output_fc: An output feature class to save the modified ws/nws features. Use this output in calc_all, below.
+    :return: Output feature class location
+    """
+
+    # Delineate overlapping regions
     arcpy.Intersect_analysis([watersheds_fc, lakes_fc], output_fc)
-    # want to drop any intersections < 10% of area of the lake, as long as it overlaps shed by 10%+, will count
+
+    # Trim the overlapping regions from the watersheds shape
+    # Want to drop any intersections < 10% of area of the lake, as long as it overlaps shed by 10%+, will count
     arcpy.Select_analysis(output_fc, 'in_memory/slivers', 'Shape_Area < (.1 * lake_waterarea_ha) * 10000')
     arcpy.Delete_management(output_fc) # used this as a temp fc so we could have Shape_Area calculated
     arcpy.Erase_analysis(watersheds_fc, 'in_memory/slivers', output_fc)
@@ -24,13 +35,20 @@ def trim_watershed_slivers(watersheds_fc, lakes_fc, output_fc):
 
 
 def calc_all(zones_fc, zone_field, lakes_fc, output_table):
+    """
+    Convenience function that runs Polygon Density in Zones for all LAGOS-US lake count metrics.
 
-    # make sure we're only using the right types of lakes, our feature
-    # class excludes everything else but this is a guarantee this will
-    # get checked at some point
+    :param zones_fc: Zones feature class
+    :param zone_field: Unique identifier for each zone
+    :param lakes_fc: Lakes feature class to be counted (LAGOS-US lake population feature class)
+    :param output_table: Output table to save the results
+    :return: None
+    """
+
+    # Ensure the lakes_fc meets our expectations for LAGOS processing: Albers USGS, contains hectares field, filtered
+    # for the appropriate Fcodes. If not, make it so.
     arcpy.env.workspace = 'in_memory'
     arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(102039)
-
     temp_lakes = 'temp_lakes'
     arcpy.CopyFeatures_management(lakes_fc, temp_lakes)
 
@@ -39,24 +57,22 @@ def calc_all(zones_fc, zone_field, lakes_fc, output_table):
         arcpy.AddField_management(temp_lakes, 'lake_waterarea_ha', 'DOUBLE')
         arcpy.CalculateField_management(temp_lakes, 'lake_waterarea_ha', '!shape.area@hectares!', 'PYTHON')
 
-    # this bit enforces the correct lake type/size restriction just in case
-    # geodata doesn't have this filtered already
     need_selection = False
     fcodes = lagosGIS.LAGOS_FCODE_LIST
-
     with arcpy.da.SearchCursor(temp_lakes, ["FCode"]) as cursor:
             for row in cursor:
                 if row[0] not in fcodes:
                     need_selection = True
-
     if need_selection:
         whereClause = '''"FCode" IN %s''' % (fcodes,)
         arcpy.Select_analysis(temp_lakes, "lakes_lagos", whereClause)
         temp_lakes = os.path.join(arcpy.env.workspace, "lakes_lagos")
 
+    # Use trimming function above if the spatial divisions provided is a watersheds layer
     if 'ws' in zones_fc:
         zones_fc = trim_watershed_slivers(zones_fc, lakes_fc, 'sliverless_sheds')
 
+    # Enumerate filtering queries and metric (table) names so that they are complete and aligned
     selections = [
             # all lake selections
             "",
@@ -159,6 +175,7 @@ def calc_all(zones_fc, zone_field, lakes_fc, output_table):
                'lakes10ha_terminallkperm'
                 ]
 
+    # Run Polygon Density in Zones for all select query & metric pairings and save to temporary tables
     for sel, temp_table in zip(selections, temp_tables):
         arcpy.AddMessage("Creating temporary table called {0} for lakes where {1}".format(temp_table, sel))
         polygon_density_in_zones.calc(zones_fc, zone_field, temp_lakes, temp_table, sel)
@@ -166,12 +183,12 @@ def calc_all(zones_fc, zone_field, lakes_fc, output_table):
         for f in new_fields:
             arcpy.AlterField_management(temp_table, f, f.replace('Poly', temp_table))
 
-    # join em up and copy to final
+    # Join up tables and copy to final output table
     temp_tables.remove('lakes1ha_all')
     for t in temp_tables:
         try:
             arcpy.JoinField_management('lakes1ha_all', zone_field, t, zone_field)
-        #sometimes there's no table if it was an empty selection
+        # sometimes there's no table if it was an empty selection
         except:
             empty_fields = [f.replace('Poly', t) for f in new_fields]
             for ef in empty_fields:
@@ -179,13 +196,13 @@ def calc_all(zones_fc, zone_field, lakes_fc, output_table):
                 arcpy.CalculateField_management('lakes1ha_all', ef, '0', 'PYTHON')
             continue
 
-    # remove all the extra zoneID fields, which have underscore in name
+    # Remove all the extra zoneID fields, which have underscore in name
     drop_fields = [f.name for f in arcpy.ListFields('lakes1ha_all') if 'zoneid_' in f.name or 'AREA' in f.name]
     for f in drop_fields:
         arcpy.DeleteField_management('lakes1ha_all', f)
     arcpy.CopyRows_management('lakes1ha_all', output_table)
 
-    # clean up
+    # Clean up
     arcpy.Delete_management('in_memory')
 
 
@@ -195,6 +212,7 @@ def main():
     lakes_fc = arcpy.GetParameterAsText(2)
     output_table = arcpy.GetParameterAsText(3)
     calc_all(zones_fc, zone_field, lakes_fc, output_table)
+
 
 if __name__ == '__main__':
     main()

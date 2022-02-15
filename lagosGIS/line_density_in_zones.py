d7e928d6fb345fd4e3ec2eca5120a9ca26b1c269
Nicole Jean Smith <nicole.j.smith@gmail.com>
2022-02-15 17:41:12 -0500

    add docstring, clean up comments

--- lagosGIS/line_density_in_zones.py
+++ lagosGIS/line_density_in_zones.py
@@ -11,6 +11,18 @@ from datetime import datetime as dt
 
 
 def calc(zones_fc, zone_field, lines_fc, out_table, where_clause='', rename_label=''):
+    """
+    Calculates areal density of line features such as streams and roads.
+    :param zones_fc: Zones feature class
+    :param zone_field: Unique identifier for each zone
+    :param lines_fc: The polyline feature class to be summarized for each zone
+    :param out_table: Output table to save the result
+    :param where_clause: (Optional) Query (SQL where clause) to filter polyline feature class before summary
+    :param rename_label: (Optional) Text string to use as prefix for all output columns
+    :return: None
+    """
+
+    # Setup: enforce coordinates, make names, filter lines if elected
     arcpy.env.workspace = 'in_memory'
     arcpy.env.outputCoordinatesystem = arcpy.SpatialReference(102039)
     if rename_label:
@@ -33,45 +45,39 @@ def calc(zones_fc, zone_field, lines_fc, out_table, where_clause='', rename_labe
             row[0] = row[1]
             cursor.updateRow(row)
 
-    # calc total length grouped by zone (numerator)
-    print("Statistics {}".format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+    # Calculate total length grouped by zone (numerator)
     lines_stat = arcpy.Statistics_analysis(lines_identity, 'lines_stat', 'length_m SUM', zone_field)
-    print("One in One out {}".format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
     lines_stat_full = lagosGIS.one_in_one_out(lines_stat, zones_fc, zone_field, 'lines_stat_full')
 
-    # get area of zones for density calc (denominator)
-    print("Zones Area {}".format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+    # Get area of zones for density calculation (denominator))
     zones_area = {}
     with arcpy.da.SearchCursor(zones_fc, [zone_field, 'SHAPE@']) as cursor:
         for row in cursor:
             zones_area[row[0]] = row[1].getArea(units='HECTARES')
 
-    # calc the density by dividing
-    print("Calc Density {}".format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+    # Calculate the density by dividing the numerator and denominator calculated and assign 0 if no lines were found
+    # in the zone
     arcpy.AddField_management(lines_stat_full, out_density_field, 'DOUBLE')
     with arcpy.da.UpdateCursor(lines_stat_full, [zone_field, out_density_field, 'SUM_length_m']) as cursor:
         for row in cursor:
             zid, mperha, msum = row
             if msum is None:
-                msum = 0 # replace NULL values with 0 which is physically accurate here
+                msum = 0  # replace NULL values with 0 which is physically accurate here
             if zid:
                 mperha = msum/zones_area[zid]
                 row = (zid, mperha, msum)
                 cursor.updateRow(row)
-            # this branch deletes ONE row that has no zone id--total length of lines not in zones
+            # this branch deletes ONE table row that has no zone id--total length of lines not in zones
             else:
                 cursor.deleteRow()
 
-    # delete extra field and ensure all input zones have output row
-    print("Cleanup {}".format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+    # Clean up
     arcpy.DeleteField_management(lines_stat_full, 'SUM_length_m')
     arcpy.DeleteField_management(lines_stat_full, 'FREQUENCY')
-
     arcpy.CopyRows_management(lines_stat_full, out_table)
-
-    # cleanup
     for item in ['lines_prep', lines_identity, lines_stat, lines_stat_full]:
         arcpy.Delete_management(item)
+
 
 def main():
     # Parameters
@@ -82,7 +88,6 @@ def main():
     where_clause = arcpy.GetParameterAsText(4)
     rename_label = arcpy.GetParameterAsText(5)
     calc(zones_fc, zone_field, lines_fc, out_table, where_clause, rename_label)
-
 
 
 if __name__ == '__main__':

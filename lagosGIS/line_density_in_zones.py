00a867c180385cae60eb5cd5284af36b516a2930
Nicole Jean Smith <nicole.j.smith@gmail.com>
2022-02-16 10:18:07 -0500

    move cracked lines summary code to function to mirror stream_density

--- lagosGIS/line_density_in_zones.py
+++ lagosGIS/line_density_in_zones.py
@@ -26,9 +26,9 @@ def calc(zones_fc, zone_field, lines_fc, out_table, where_clause='', rename_labe
     arcpy.env.workspace = 'in_memory'
     arcpy.env.outputCoordinatesystem = arcpy.SpatialReference(102039)
     if rename_label:
-        out_density_field = '{}_mperha'.format(rename_label)
+        density_field_name = '{}_mperha'.format(rename_label)
     else:
-        out_density_field = '{}_mperha'.format(os.path.basename(lines_fc))
+        density_field_name = '{}_mperha'.format(os.path.basename(lines_fc))
 
     if where_clause:
         lines_prep = arcpy.Select_analysis(lines_fc, 'lines_prep', where_clause)
@@ -45,37 +45,48 @@ def calc(zones_fc, zone_field, lines_fc, out_table, where_clause='', rename_labe
             row[0] = row[1]
             cursor.updateRow(row)
 
-    # Calculate total length grouped by zone (numerator)
-    lines_stat = arcpy.Statistics_analysis(lines_identity, 'lines_stat', 'length_m SUM', zone_field)
-    lines_stat_full = lagosGIS.one_in_one_out(lines_stat, zones_fc, zone_field, 'lines_stat_full')
+    def summarize_cracked(cracked_lines, density_field_name):
+        # Calculate total length grouped by zone (numerator)
+        lines_stat = arcpy.Statistics_analysis(cracked_lines, 'lines_stat', 'length_m SUM', zone_field)
+        lines_stat_full = lagosGIS.one_in_one_out(lines_stat, zones_fc, zone_field, 'lines_stat_full')
 
-    # Get area of zones for density calculation (denominator))
-    zones_area = {}
-    with arcpy.da.SearchCursor(zones_fc, [zone_field, 'SHAPE@']) as cursor:
-        for row in cursor:
-            zones_area[row[0]] = row[1].getArea(units='HECTARES')
+        # Get area of zones for density calc (denominator)
+        zones_area = {}
+        with arcpy.da.SearchCursor(zones_fc, [zone_field, 'SHAPE@']) as cursor:
+            for row in cursor:
+                zones_area[row[0]] = row[1].getArea(units='HECTARES')
 
-    # Calculate the density by dividing the numerator and denominator calculated and assign 0 if no lines were found
-    # in the zone
-    arcpy.AddField_management(lines_stat_full, out_density_field, 'DOUBLE')
-    with arcpy.da.UpdateCursor(lines_stat_full, [zone_field, out_density_field, 'SUM_length_m']) as cursor:
-        for row in cursor:
-            zid, mperha, msum = row
-            if msum is None:
-                msum = 0  # replace NULL values with 0 which is physically accurate here
-            if zid:
-                mperha = msum/zones_area[zid]
-                row = (zid, mperha, msum)
-                cursor.updateRow(row)
-            # this branch deletes ONE table row that has no zone id--total length of lines not in zones
-            else:
-                cursor.deleteRow()
+        # Calculate the density by dividing numerator by denominator
+        arcpy.AddField_management(lines_stat_full, density_field_name, 'DOUBLE')
+        with arcpy.da.UpdateCursor(lines_stat_full, [zone_field, density_field_name, 'SUM_length_m']) as cursor:
+            for row in cursor:
+                zid, mperha, msum = row
+                if msum is None:
+                    msum = 0  # replace NULL values with 0 which is physically accurate here
+                if zid:
+                    mperha = msum/zones_area[zid]
+                    row = (zid, mperha, msum)
+                    cursor.updateRow(row)
+                # this branch deletes ONE row that has no zone id--total length of lines not in zones
+                else:
+                    cursor.deleteRow()
 
-    # Clean up
-    arcpy.DeleteField_management(lines_stat_full, 'SUM_length_m')
-    arcpy.DeleteField_management(lines_stat_full, 'FREQUENCY')
-    arcpy.CopyRows_management(lines_stat_full, out_table)
-    for item in ['lines_prep', lines_identity, lines_stat, lines_stat_full]:
+        # Delete extra fields
+        arcpy.DeleteField_management(lines_stat_full, 'SUM_length_m')
+        arcpy.DeleteField_management(lines_stat_full, 'FREQUENCY')
+
+        # Copy output to an in_memory fc with same name as output field so we can delete lines_stat_full
+        summarized = arcpy.CopyRows_management(lines_stat_full, density_field_name)
+
+        # Cleanup
+        for item in [lines_stat, lines_stat_full]:
+            arcpy.Delete_management(item)
+        return summarized
+
+    # Run summary, copy to output, clean up
+    summary = summarize_cracked(lines_identity, density_field_name)
+    arcpy.CopyRows_management(summary, out_table)
+    for item in ['lines_prep', lines_identity]:
         arcpy.Delete_management(item)
 
 
